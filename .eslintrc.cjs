866b808622388f1dab461ad805746150b61d42bd
Masoud Bonabi <m.bonabi@snapp.express>
2023-04-02 16:24:00 +0430

    Fixes panel's subdomain

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -26,7 +26,7 @@ module.exports = {
   rules: {
     "jsx-a11y/no-static-element-interactions": "off",
     "jsx-a11y/click-events-have-key-events": "off",
-    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-explicit-any": "warn",
     "jsx-a11y/no-noninteractive-element-interactions": "off",
 
     // "tailwindcss/no-custom-classname": [
