ee4696313916d08da09902b9621f82ae6cd4a671
arash16 <arash.shakery@gmail.com>
2023-08-17 14:53:56 +0330

    refactor: cleanup routes, remove react-date-range

--- src/modules/base/App/App.tsx
+++ src/modules/base/App/App.tsx
@@ -1,39 +1,16 @@
-import * as React from 'react';
 import { useEffect } from 'react';
 import ReactGA from 'react-ga4';
 import { hotjar } from 'react-hotjar';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import 'tw-elements';
-
-import DashboardPage from 'modules/wallet/DashboardPage';
 
 import ConfirmSignUp from 'modules/auth/ConfirmSignUp';
 import { SecondaryForm } from 'modules/auth/SecondaryForm';
-import 'react-date-range/dist/styles.css';
-import 'react-date-range/dist/theme/default.css';
 import { useInvestorAssetStructuresQuery, useUserInfoQuery } from 'api';
-import { PageWrapper } from '../PageWrapper';
 import { Splash } from '../Splash';
-import { Container } from '../layout/Container';
 import './App.css';
 import './tailwind.css';
-
-const SignalsMatrixPage = React.lazy(
-  () => import('modules/products/SignalsMatrixPage'),
-);
-
-const ProductsCatalogPage = React.lazy(
-  () => import('modules/products/ProductsCatalogPage'),
-);
-const ProductCatalogDetailPage = React.lazy(
-  () => import('modules/products/ProductCatalogDetailPage'),
-);
-
-const AssetOverviewPage = React.lazy(
-  () => import('modules/wallet/AssetOverviewPage'),
-);
-const WithdrawPage = React.lazy(() => import('modules/wallet/WithdrawPage'));
-const DepositPage = React.lazy(() => import('modules/wallet/DepositPage'));
+import AppRoutes from './AppRoutes';
 
 export const App = () => {
   const userInfo = useUserInfoQuery();
@@ -69,75 +46,10 @@ export const App = () => {
     return <ConfirmSignUp />;
   }
 
-  const dashboardElement = <DashboardPage />;
-
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route element={<Container />}>
-            <Route path="/" element={<Navigate to="/app" />} />
-            <Route path="login/:action" element={dashboardElement} />
-            <Route path="app" element={<Navigate to="/app/dashboard" />} />
-            <Route path="app/:section" element={dashboardElement} />
-            <Route path="app/:section/:subsection" element={dashboardElement} />
-            <Route path="/auth/callback" element={<Navigate to="/app" />} />
-
-            <Route
-              path="app/products-catalog"
-              element={
-                <React.Suspense fallback={<PageWrapper loading />}>
-                  <ProductsCatalogPage />
-                </React.Suspense>
-              }
-            />
-
-            <Route
-              path="app/products-catalog/:fpKey"
-              element={
-                <React.Suspense fallback={<PageWrapper loading />}>
-                  <ProductCatalogDetailPage />
-                </React.Suspense>
-              }
-            />
-
-            <Route
-              path="app/signals"
-              element={
-                <React.Suspense fallback={<PageWrapper loading />}>
-                  <SignalsMatrixPage />
-                </React.Suspense>
-              }
-            />
-
-            <Route
-              path="app/withdraw"
-              element={
-                <React.Suspense fallback={<PageWrapper loading />}>
-                  <WithdrawPage />
-                </React.Suspense>
-              }
-            />
-
-            <Route
-              path="app/asset-overview"
-              element={
-                <React.Suspense fallback={<PageWrapper loading />}>
-                  <AssetOverviewPage />
-                </React.Suspense>
-              }
-            />
-
-            <Route
-              path="app/deposit/:exchangeAccountKey"
-              element={
-                <React.Suspense fallback={<PageWrapper loading />}>
-                  <DepositPage />
-                </React.Suspense>
-              }
-            />
-          </Route>
-        </Routes>
+        <AppRoutes />
       </BrowserRouter>
     </>
   );
---
+++ src/modules/base/App/AppRoutes.tsx
@@ -0,0 +1,93 @@
+import * as React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import DashboardPage from 'modules/wallet/DashboardPage';
+import { PageWrapper } from '../PageWrapper';
+import { Container } from '../layout/Container';
+
+const SignalsMatrixPage = React.lazy(
+  () => import('modules/products/SignalsMatrixPage'),
+);
+
+const ProductsCatalogPage = React.lazy(
+  () => import('modules/products/ProductsCatalogPage'),
+);
+const ProductCatalogDetailPage = React.lazy(
+  () => import('modules/products/ProductCatalogDetailPage'),
+);
+
+const AssetOverviewPage = React.lazy(
+  () => import('modules/wallet/AssetOverviewPage'),
+);
+const WithdrawPage = React.lazy(() => import('modules/wallet/WithdrawPage'));
+const DepositPage = React.lazy(() => import('modules/wallet/DepositPage'));
+
+export default function AppRoutes() {
+  const dashboardElement = <DashboardPage />;
+
+  return (
+    <Routes>
+      <Route element={<Container />}>
+        <Route path="/" element={<Navigate to="/app" />} />
+        <Route path="login/:action" element={dashboardElement} />
+        <Route path="app" element={<Navigate to="/app/dashboard" />} />
+        <Route path="app/:section" element={dashboardElement} />
+        <Route path="app/:section/:subsection" element={dashboardElement} />
+        <Route path="/auth/callback" element={<Navigate to="/app" />} />
+
+        <Route
+          path="app/products-catalog"
+          element={
+            <React.Suspense fallback={<PageWrapper loading />}>
+              <ProductsCatalogPage />
+            </React.Suspense>
+          }
+        />
+
+        <Route
+          path="app/products-catalog/:fpKey"
+          element={
+            <React.Suspense fallback={<PageWrapper loading />}>
+              <ProductCatalogDetailPage />
+            </React.Suspense>
+          }
+        />
+
+        <Route
+          path="app/signals"
+          element={
+            <React.Suspense fallback={<PageWrapper loading />}>
+              <SignalsMatrixPage />
+            </React.Suspense>
+          }
+        />
+
+        <Route
+          path="app/withdraw"
+          element={
+            <React.Suspense fallback={<PageWrapper loading />}>
+              <WithdrawPage />
+            </React.Suspense>
+          }
+        />
+
+        <Route
+          path="app/asset-overview"
+          element={
+            <React.Suspense fallback={<PageWrapper loading />}>
+              <AssetOverviewPage />
+            </React.Suspense>
+          }
+        />
+
+        <Route
+          path="app/deposit/:exchangeAccountKey"
+          element={
+            <React.Suspense fallback={<PageWrapper loading />}>
+              <DepositPage />
+            </React.Suspense>
+          }
+        />
+      </Route>
+    </Routes>
+  );
+}
