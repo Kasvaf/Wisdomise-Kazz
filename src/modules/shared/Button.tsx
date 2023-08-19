43671350a9be4834f691cffc6aebd64027976974
arash16 <arash.shakery@gmail.com>
2023-08-19 18:57:03 +0330

    Button: better handling of loading/disabled

--- src/modules/shared/Button.tsx
+++ src/modules/shared/Button.tsx
@@ -1,6 +1,7 @@
 import { clsx } from 'clsx';
 import type React from 'react';
-import { type DetailedHTMLProps } from 'react';
+import { useCallback, type DetailedHTMLProps } from 'react';
+import Spin from './Spin';
 
 interface Props
   extends DetailedHTMLProps<
@@ -19,9 +20,21 @@ export const Button: React.FC<Props> = ({
   children,
   className,
   disabled,
+  onClick,
   ...restOfProps
 }) => {
-  const btnContent = loading ? 'Loading . . .' : children;
+  const btnContent = (
+    <div className="flex items-center justify-center">
+      {loading && <Spin className="mr-2" />}
+      {children}
+    </div>
+  );
+  const clickHandler: React.MouseEventHandler<HTMLButtonElement> = useCallback(
+    ev => {
+      if (!loading && !disabled) onClick?.(ev);
+    },
+    [loading, disabled, onClick],
+  );
 
   if (variant === 'secondary') {
     return (
@@ -29,9 +42,11 @@ export const Button: React.FC<Props> = ({
         className={clsx(
           'rounded-[40px] border border-white bg-transparent px-8 py-4 text-sm font-medium leading-none text-white hover:border-white/40',
           size === 'small' && '!p-[10px_12px] ',
+          loading && 'cursor-wait',
           className,
         )}
         disabled={disabled}
+        onClick={clickHandler}
         {...restOfProps}
       >
         {btnContent}
@@ -46,9 +61,11 @@ export const Button: React.FC<Props> = ({
           'rounded-[40px] bg-white/10 px-8 py-4 text-sm font-medium leading-none text-white hover:bg-black/5',
           disabled &&
             'bg-white/10 text-white/10 hover:cursor-default hover:!bg-white/10',
+          loading && 'cursor-wait',
           className,
         )}
         disabled={disabled}
+        onClick={clickHandler}
         {...restOfProps}
       >
         {btnContent}
@@ -63,11 +80,12 @@ export const Button: React.FC<Props> = ({
           'bg-transparent px-8 py-4 text-sm font-medium leading-none text-white',
           disabled &&
             'bg-white/10 text-white/10 hover:cursor-default hover:!bg-white/10',
-
+          loading && 'cursor-wait',
           size === 'small' && '!p-[10px_12px]',
           className,
         )}
         disabled={disabled}
+        onClick={clickHandler}
         {...restOfProps}
       >
         {btnContent}
@@ -81,11 +99,12 @@ export const Button: React.FC<Props> = ({
         'rounded-[40px] bg-white px-8 py-4 text-sm font-medium leading-none text-black hover:bg-white/80',
         disabled &&
           'bg-white/10 text-white/10 hover:cursor-default hover:!bg-white/10',
-
+        loading && 'cursor-wait',
         size === 'small' && '!p-[10px_12px]',
         className,
       )}
       disabled={disabled}
+      onClick={clickHandler}
       {...restOfProps}
     >
       {btnContent}
---
+++ src/modules/shared/Spin.tsx
@@ -0,0 +1,11 @@
+import { LoadingOutlined } from '@ant-design/icons';
+import { Spin as Spinner } from 'antd';
+
+const Spin: React.FC<{ className?: string }> = ({ className }) => (
+  <Spinner
+    className={className}
+    indicator={<LoadingOutlined style={{ fontSize: 14 }} spin rev={false} />}
+  />
+);
+
+export default Spin;
