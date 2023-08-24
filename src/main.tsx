8941b5e0427f2be4bf3eae14c20d7c7ec69e5fad
arash16 <arash.shakery@gmail.com>
2023-08-24 11:05:38 +0330

    better design for error page

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import configApp from 'config/config';
 import queryClient from 'config/reactQuery';
 import AppAuthContainer from 'modules/base/AppAuthContainer';
+import ErrorPage from 'modules/base/ErrorPage';
 
 configApp();
 
@@ -11,17 +12,9 @@ const root = document.querySelector('#root');
 if (!root) throw new Error('unexpected');
 
 createRoot(root).render(
-  <Sentry.ErrorBoundary fallback={<SentryErrorFallback />}>
+  <Sentry.ErrorBoundary fallback={<ErrorPage />}>
     <QueryClientProvider client={queryClient}>
       <AppAuthContainer />
     </QueryClientProvider>
   </Sentry.ErrorBoundary>,
 );
-
-function SentryErrorFallback() {
-  return (
-    <div className="flex h-screen w-screen items-center justify-center text-white">
-      Error Occurred... Sorry!
-    </div>
-  );
-}
