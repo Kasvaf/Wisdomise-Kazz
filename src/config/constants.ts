6c8b4b41935c33e03055fc8892f26ec599a9e2df
arash16 <arash.shakery@gmail.com>
2023-08-31 12:36:18 +0330

    fix account's app-name for new sub-domain

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,12 +1,17 @@
 import { isProduction } from 'utils/version';
 
 export const JwtTokenKey = 'TOKEN';
-export const appName = window.location.host.includes('localhost')
-  ? 'local-'
-  : '';
+
+const { hostname } = window.location;
+const appName =
+  hostname === 'localhost'
+    ? 'local-dashboard'
+    : hostname.endsWith('app.wisdomise.io')
+    ? 'dashboard'
+    : 'wealth';
 
 const accountOrigin = `https://${
   isProduction ? '' : 'stage-'
 }account.wisdomise.io`;
-export const LoginUrl = `${accountOrigin}/auth/login?app=${appName}wealth`;
-export const LogoutUrl = `${accountOrigin}/auth/logout?app=${appName}wealth`;
+export const LoginUrl = `${accountOrigin}/auth/login?app=${appName}`;
+export const LogoutUrl = `${accountOrigin}/auth/logout?app=${appName}`;
