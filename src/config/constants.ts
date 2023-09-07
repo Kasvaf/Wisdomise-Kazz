2c3b8eaada019b3ad731ab09dd87205d6f639f5e
arash16 <arash.shakery@gmail.com>
2023-09-07 11:47:07 +0330

    dynamic domain handling (.io to .com)

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,19 +1,31 @@
 import { isProduction } from 'utils/version';
 
-export const RouterBaseName = (import.meta.env.VITE_BRANCH as string) || '';
-
 const { hostname } = window.location;
+const Domain =
+  hostname === 'localhost'
+    ? 'wisdomise.io'
+    : hostname.replace(/^(?:[\w-]+\.)*([\w-]+\.\w+)$/, '$1');
+const subdomainPrefix = isProduction ? '' : 'stage-';
+
+const makeOrigin = (name: string) =>
+  `https://${subdomainPrefix}${name}.${Domain}`;
+
+export const API_ORIGIN = makeOrigin('api');
+export const STRATEGY_ORIGIN = makeOrigin('strategy');
+export const TEMPLE_ORIGIN = makeOrigin('temple');
+export const ATHENA_FE = makeOrigin('athena');
+export const ACCOUNT_ORIGIN = makeOrigin('account');
+
+// account info:
+
 const appName =
   hostname === 'localhost'
     ? 'local-dashboard'
-    : hostname.endsWith('app.wisdomise.io')
+    : hostname.endsWith(`app.${Domain}`)
     ? 'dashboard'
     : 'wealth';
 
-const accountOrigin = `https://${
-  isProduction ? '' : 'stage-'
-}account.wisdomise.io`;
-
 export const JwtTokenKey = 'TOKEN';
-export const LoginUrl = `${accountOrigin}/auth/login?app=${appName}&v=${RouterBaseName}`;
-export const LogoutUrl = `${accountOrigin}/auth/logout?app=${appName}&v=${RouterBaseName}`;
+export const RouterBaseName = (import.meta.env.VITE_BRANCH as string) || '';
+export const LoginUrl = `${ACCOUNT_ORIGIN}/auth/login?app=${appName}&v=${RouterBaseName}`;
+export const LogoutUrl = `${ACCOUNT_ORIGIN}/auth/logout?app=${appName}&v=${RouterBaseName}`;
