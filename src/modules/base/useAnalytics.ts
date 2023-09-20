9d8e19d77007938d056d4464d640b1f08c0d89a6
arash16 <arash.shakery@gmail.com>
2023-09-20 15:27:26 +0330

    cleanup useAnalytics

--- src/modules/base/useAnalytics.ts
+++ src/modules/base/useAnalytics.ts
@@ -5,11 +5,15 @@ import { hotjar } from 'react-hotjar';
 export default function useAnalytics() {
   // ** hotjar and GA config
   useEffect(() => {
-    if (import.meta.env.VITE_HJID && import.meta.env.VITE_HJSV) {
-      hotjar.initialize(import.meta.env.VITE_HJID, import.meta.env.VITE_HJSV);
+    const hjid = import.meta.env.VITE_HJID;
+    const hjsv = import.meta.env.VITE_HJSV;
+    if (hjid && hjsv) {
+      hotjar.initialize(hjid, hjsv);
     }
-    if (import.meta.env.VITE_GA) {
-      ReactGA.initialize(import.meta.env.VITE_GA);
+
+    const gaId = import.meta.env.VITE_GA;
+    if (gaId) {
+      ReactGA.initialize(gaId);
     }
   }, []);
 }
