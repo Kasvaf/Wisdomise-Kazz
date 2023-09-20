f37ecd99cabdd8a0e27c61bc0b6e0acbd2646a0c
arash16 <arash.shakery@gmail.com>
2023-09-20 22:38:40 +0330

    inline splash image

--- src/modules/base/Splash.tsx
+++ src/modules/base/Splash.tsx
@@ -1,9 +1,9 @@
-import Logo from 'assets/logo-vertical.svg';
+import { ReactComponent as Logo } from 'assets/logo-vertical.svg';
 
 const Splash = () => {
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center">
-      <img className="mb-[100px] h-[200px] w-full" src={Logo} alt="logo" />
+      <Logo className="mb-[50px] h-[200px] w-full" />
       <div className="loader-line" />
       <p className="mt-[14px] text-[16px] font-medium text-white">
         Wisdomise is loading ...
