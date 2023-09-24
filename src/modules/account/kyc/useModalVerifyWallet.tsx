1a38095e29ec6d7bf1b11dbef228ce3cc4dbbf7d
arash16 <arash.shakery@gmail.com>
2023-09-24 15:19:14 +0330

    fix typo

--- src/modules/account/kyc/useModalVerifyWallet.tsx
+++ src/modules/account/kyc/useModalVerifyWallet.tsx
@@ -58,8 +58,7 @@ const ModalVerifyWallet: React.FC<{ onResolve?: () => void }> = () => (
       </div>
       <div className="flex flex-col justify-center rounded-3xl bg-[#4C5059] p-4 pt-2 text-[12px] md:basis-1/2">
         <div className="mb-2 text-center font-normal">
-          <strong className="font-medium">E.g.</strong> : USDT wallet address in
-          TRC20
+          <strong className="font-medium">Example</strong>: USDT Wallet Address
         </div>
         <img src={screenshot} />
       </div>
