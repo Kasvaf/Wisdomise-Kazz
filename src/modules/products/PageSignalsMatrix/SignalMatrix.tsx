266425f85f70d63a9b885a3a467c52193d61733f
arash16 <arash.shakery@gmail.com>
2023-09-27 14:34:52 +0330

    refactor SignalMatrix to separate component

---
+++ src/modules/products/PageSignalsMatrix/SignalMatrix.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import { styled } from '@linaria/react';
+import { type SignalsResponse } from 'api/types/signalResponse';
+import PriceAreaChart from 'shared/PriceAreaChart';
+import PriceChange from 'shared/PriceChange';
+import PairInfo from 'shared/PairInfo';
+import SignalBox from './SignalBox';
+
+const EmptySignal = styled.div`
+  @apply h-full w-full rounded-lg bg-[#131822];
+  background-image: repeating-linear-gradient(
+    45deg,
+    transparent,
+    transparent 15px,
+    rgba(255, 255, 255, 0.05) 15px,
+    rgba(255, 255, 255, 0.05) 30px
+  );
+`;
+
+const SignalMatrix: React.FC<{ signals: SignalsResponse }> = ({ signals }) => {
+  return (
+    <div
+      className="grid w-min rounded-3xl bg-white/5 px-2 py-4 pb-2"
+      style={{
+        gridTemplateColumns: `max-content max-content repeat(${signals.strategies.length},max-content)`,
+      }}
+    >
+      {[
+        'Pairs',
+        '24h %',
+        ...(signals.strategies.map(s => [
+          s.title,
+          s.position_sides.join(', '),
+        ]) ?? []),
+      ].map(e => (
+        <div
+          key={e.toString()}
+          className="border-b border-white/10 py-2 text-center text-xs font-normal text-white"
+        >
+          {Array.isArray(e) ? (
+            <span>
+              {e[0]} <span className="text-white/40">({e[1]})</span>
+            </span>
+          ) : (
+            e
+          )}
+        </div>
+      ))}
+
+      {signals.pairs.map(pair => (
+        <React.Fragment key={pair.name}>
+          <PairInfo
+            title={pair.title}
+            base={pair.base_name}
+            quote={pair.quote_name}
+          />
+          <div className="flex flex-col items-center justify-center p-2">
+            <PriceChange value={pair.time_window_pnl} className="mb-2" />
+            <div className="w-36">
+              <PriceAreaChart
+                data={pair.time_window_prices.map((d, i) => ({
+                  x: i,
+                  y: d,
+                }))}
+              />
+            </div>
+          </div>
+          {signals.strategies.map(strategy => {
+            const position = signals.last_positions.find(
+              p =>
+                p.strategy_name === strategy.name && pair.name === p.pair_name,
+            );
+
+            return (
+              <div
+                key={strategy.name}
+                className="min-h-[149px] min-w-[170px] p-2"
+              >
+                {position == null ? (
+                  <EmptySignal />
+                ) : (
+                  <SignalBox position={position} />
+                )}
+              </div>
+            );
+          })}
+        </React.Fragment>
+      ))}
+    </div>
+  );
+};
+
+export default SignalMatrix;
