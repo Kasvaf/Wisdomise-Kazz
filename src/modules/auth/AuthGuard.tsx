0a5b3b85be3f482404e09bfb3b86998857834111
arash16 <arash.shakery@gmail.com>
2023-10-22 16:47:12 +0330

    fix stage with no-segment scripts

--- src/modules/auth/AuthGuard.tsx
+++ src/modules/auth/AuthGuard.tsx
@@ -27,6 +27,19 @@ export default function AuthGuard({ children }: PropsWithChildren) {
   const navigate = useNavigate();
   const { data: account } = useAccountQuery();
 
+  const email = account?.email;
+  useEffect(() => {
+    if (email) {
+      const { analytics } = window as any;
+      if (analytics) {
+        analytics.identify(email, {
+          userId: email,
+          email,
+        });
+      }
+    }
+  }, [email]);
+
   const appsInfo = useAppsInfoQuery();
   const apps = appsInfo.data?.results;
 
@@ -76,16 +89,6 @@ export default function AuthGuard({ children }: PropsWithChildren) {
       }
     }
   }, [loading, account, appsInfo, navigate, handleAppRedirect, redirectLogin]);
-
-  const email = account?.email;
-  useEffect(() => {
-    if (email) {
-      (window as any).analytics.identify(email, {
-        userId: email,
-        email,
-      });
-    }
-  }, [email]);
 
   return loading ? <Splash /> : <>{children}</>;
 }
