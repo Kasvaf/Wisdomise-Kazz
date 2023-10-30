36d6c288c0c1dcdf6c1aa7b6605b07cf6cd93cfd
arash16 <arash.shakery@gmail.com>
2023-10-30 13:30:19 +0330

    update ip-access-restriction to copy-text-box

--- src/modules/account/useModalAddExchangeAccount.tsx
+++ src/modules/account/useModalAddExchangeAccount.tsx
@@ -7,7 +7,7 @@ import useModal from 'shared/useModal';
 import TextBox from 'shared/TextBox';
 import Button from 'shared/Button';
 import LabelInfo from 'modules/shared/LabelInfo';
-import Banner from 'modules/shared/Banner';
+import CopyInputBox from 'modules/shared/CopyInputBox';
 import ExchangeSelector from './ExchangeSelector';
 import MarketSelector from './MarketSelector';
 
@@ -108,18 +108,16 @@ const ModalAddExchangeAccount: React.FC<{
         />
       </div>
 
-      <Banner className="mt-8">
-        <div className="mb-2 text-white/80">
-          Use following values for{' '}
+      <div className="mt-8 rounded-3xl">
+        <div className="my-2 ml-2 text-white/80">
+          Use following value for{' '}
           <code className="text-white">IP access restrictions</code> in Binance:
         </div>
-        <ul className="ml-4 list-disc">
-          <li>13.39.30.245</li>
-          <li>13.36.152.59</li>
-          <li>13.36.112.70</li>
-          <li>15.188.188.103</li>
-        </ul>
-      </Banner>
+        <CopyInputBox
+          style="alt"
+          value="13.39.30.245 13.36.152.59 13.36.112.70 15.188.188.103"
+        />
+      </div>
 
       <div className="mt-8 flex justify-center">
         <Button onClick={addHandler} loading={isSubmitting}>
