9ec76f0b7c007dd2f168c216491a618f03338a55
arash16 <arash.shakery@gmail.com>
2023-11-14 10:34:21 +0330

    fix sentry endpoint: .io to .com

--- src/config/sentry.ts
+++ src/config/sentry.ts
@@ -5,7 +5,7 @@ import { isLocal } from 'utils/version';
 export default function configSentry() {
   if (!isLocal) {
     Sentry.init({
-      dsn: 'https://ee6306165f3f4aef867f1fa37bcbf494@sentry.wisdomise.io/9',
+      dsn: 'https://ee6306165f3f4aef867f1fa37bcbf494@sentry.wisdomise.com/9',
       integrations: [new BrowserTracing()],
       // We recommend adjusting this value in production, or using tracesSampler
       // for finer control
