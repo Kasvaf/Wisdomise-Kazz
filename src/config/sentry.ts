befd110b11523b1953ba267597d9a1275d4a4ebe
arash16 <arash.shakery@gmail.com>
2023-11-14 14:40:16 +0330

    config new sentry

--- src/config/sentry.ts
+++ src/config/sentry.ts
@@ -5,7 +5,7 @@ import { isLocal } from 'utils/version';
 export default function configSentry() {
   if (!isLocal) {
     Sentry.init({
-      dsn: 'https://ee6306165f3f4aef867f1fa37bcbf494@sentry.wisdomise.com/9',
+      dsn: 'https://79fe673a0ca923d0ae50321a49d10eec@sentry.wisdomise.com/13',
       integrations: [new BrowserTracing()],
       // We recommend adjusting this value in production, or using tracesSampler
       // for finer control
