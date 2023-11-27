6fcfe1c73ceefa9486878ad561142f73a3e6ab27
Majid Pouramini <pouramini.majid@gmail.com>
2023-11-27 17:56:46 +0330

    add translation for wsdm page

--- src/modules/account/PageToken/index.tsx
+++ src/modules/account/PageToken/index.tsx
@@ -12,7 +12,7 @@ import { ReactComponent as WalletIcon } from './images/wallet.svg';
 import { ReactComponent as UtilityIcon } from './images/utility.svg';
 
 export default function PageToken() {
-  const { t } = useTranslation();
+  const { t } = useTranslation('token');
   const { isActive, isLoading, isTrialPlan } = useSubscription();
   const { data: account } = useAccountQuery();
   const navigate = useNavigate();
@@ -42,7 +42,7 @@ export default function PageToken() {
             <div className="flex justify-between">
               <div>
                 <h1 className="mb-6 text-xl font-semibold">
-                  Your tWSDM token balance
+                  {t('balance.your-balance')}
                 </h1>
                 <Button variant="secondary" onClick={updateTokenBalance}>
                   {t('billing:token-modal.refresh')}
@@ -52,36 +52,35 @@ export default function PageToken() {
             </div>
             <div className="flex items-center justify-between pt-8">
               <span className="text-3xl font-bold">
-                {addComma(account?.wsdm_balance ?? 0)} tWSDM
+                {addComma(account?.wsdm_balance ?? 0)} {t('balance.token-name')}
               </span>
-              <Button onClick={openInvestmentPanel}>Add tWSDM</Button>
+              <Button onClick={openInvestmentPanel}>
+                {t('balance.add-token')}
+              </Button>
             </div>
           </Card>
           <Card>
             <div className="flex justify-between">
               <div>
                 <h1 className="mb-6 text-xl font-semibold">
-                  Utility Activation
+                  {t('utility.title')}
                 </h1>
                 <p className="text-xs text-white/60">
-                  Unlock exclusive access to Wisdomise’s premium subscription
-                  packages for an entire year just by holding onto your $tWSDM
-                  tokens. There’s no deduction from your tokens – simply keep
-                  them in your wallet. This effortless approach ensures you
-                  enjoy all the premium benefits without any additional cost or
-                  hassle. It’s a smart way to maximize the value of your
-                  investment while exploring the full range of services that
-                  Wisdomise has to offer.
+                  {t('utility.description')}
                 </p>
               </div>
               <UtilityIcon className="shrink-0" />
             </div>
             <div className="flex items-center justify-between pt-8">
               <span className="text-3xl font-bold">
-                {isActive && !isTrialPlan ? 'Activated' : 'Not Activated'}
+                {isActive && !isTrialPlan
+                  ? t('utility.activated')
+                  : t('utility.not-activated')}
               </span>
               {(!isActive || isTrialPlan) && (
-                <Button onClick={openBillingPage}>Activate</Button>
+                <Button onClick={openBillingPage}>
+                  {t('utility.activate')}
+                </Button>
               )}
             </div>
           </Card>
