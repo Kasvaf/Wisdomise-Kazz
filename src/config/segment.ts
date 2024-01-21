d56876a4836006fa12072361d1421c589b876f7c
arash16 <arash.shakery@gmail.com>
2024-01-21 21:55:28 +0330

    add segment key for staging

--- src/config/segment.ts
+++ src/config/segment.ts
@@ -1,12 +1,14 @@
 import { AnalyticsBrowser } from '@segment/analytics-next';
-import { isProduction } from 'utils/version';
+import { isLocal, isProduction } from 'utils/version';
 
 export const analytics = new AnalyticsBrowser();
 
 export function configSegment() {
-  if (isProduction) {
+  if (!isLocal) {
     void analytics.load({
-      writeKey: 'KziKXYCVqvj5eaHjp2rYzsuCpElkH04I',
+      writeKey: isProduction
+        ? 'KziKXYCVqvj5eaHjp2rYzsuCpElkH04I'
+        : 'iKtBnPRu3zSacgdBFQBh9GuKiPhoADEz',
     });
   }
 }
