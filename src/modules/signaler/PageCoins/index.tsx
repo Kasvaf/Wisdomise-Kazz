4df273b55a818021a9f5e3c1bfa7d7e9d0ad5f24
arash16 <arash.shakery@gmail.com>
2024-02-11 01:13:35 +0330

    add notif icons

--- src/modules/signaler/PageCoins/index.tsx
+++ src/modules/signaler/PageCoins/index.tsx
@@ -1,7 +1,7 @@
 import PageWrapper from 'modules/base/PageWrapper';
 import { useSignalerPairs } from 'api/signaler';
 import { useRecentCandlesQuery } from 'api';
-import useSearchParamAsState from 'modules/shared/useSearchParamAsState';
+import useSearchParamAsState from 'shared/useSearchParamAsState';
 import CandleChart from 'modules/strategy/PageStrategyDetails/TabPositions/CandleChart';
 import CoinSelector from '../CoinSelector';
 import CoinOverview from './CoinOverview';
@@ -26,7 +26,7 @@ export default function PageCoins() {
           loading={coins.isLoading}
           selectedItem={coin}
           onSelect={c => setCoinName(c.name)}
-          className="mb-8 w-[300px] mobile:w-full"
+          className="mb-8 w-[320px] mobile:w-full"
         />
 
         {coin && (
@@ -39,7 +39,7 @@ export default function PageCoins() {
               <h2 className="mb-3 text-2xl font-semibold">Signals Overview</h2>
               <p className="text-sm">
                 Check Detail of any Strategy , also you can turn on notification
-                by click on bell icon.
+                by clicking on bell icon.
               </p>
             </div>
             <CoinSignalersList coin={coin} />
--- src/modules/signaler/PageSignaler/index.tsx
+++ src/modules/signaler/PageSignaler/index.tsx
@@ -1,12 +1,34 @@
+import { clsx } from 'clsx';
+import { type PropsWithChildren } from 'react';
+import { bxBell } from 'boxicons-quasar';
 import PageWrapper from 'modules/base/PageWrapper';
 import { useStrategyPositions, useStrategiesList } from 'api/signaler';
 import Spinner from 'modules/shared/Spinner';
 import useSearchParamAsState from 'modules/shared/useSearchParamAsState';
+import Button from 'modules/shared/Button';
+import Icon from 'modules/shared/Icon';
 import CoinSelector from '../CoinSelector';
 import ActivePosition from '../ActivePosition';
 import StrategySelector from './StrategySelector';
 import SimulatedPositions from './SimulatedPositions';
 import SimulatedChart from './SimulatedChart';
+
+const FieldTitle: React.FC<
+  PropsWithChildren<{
+    title: string;
+    description: string;
+    className?: string;
+  }>
+> = ({ title, description, children, className }) => {
+  return (
+    <div className={clsx('flex flex-col', className)}>
+      <div className="mb-1 text-xl font-semibold text-white/40">{title}</div>
+      <div className="mb-3 text-sm text-white/20">{description}</div>
+      <div className="grow" />
+      {children}
+    </div>
+  );
+};
 
 export default function PageCoins() {
   const strategies = useStrategiesList();
@@ -28,23 +50,45 @@ export default function PageCoins() {
   return (
     <PageWrapper loading={false}>
       <div>
-        <div className="flex items-center gap-4">
-          <StrategySelector
-            strategies={strategies.data}
-            loading={strategies.isLoading}
-            selectedItem={strategy}
-            onSelect={s => setStrategyKey(s.key)}
-            className="mb-8 w-[300px]"
-          />
+        <div className="mb-8 flex flex-wrap items-stretch gap-4 mobile:flex-col">
+          <FieldTitle
+            title="Strategy"
+            description="Choose Strategy then choose from crypto list"
+            className="w-[320px] mobile:w-full"
+          >
+            <StrategySelector
+              strategies={strategies.data}
+              loading={strategies.isLoading}
+              selectedItem={strategy}
+              onSelect={s => setStrategyKey(s.key)}
+            />
+          </FieldTitle>
 
           {strategy && (
-            <CoinSelector
-              coins={strategy.supported_pairs}
-              selectedItem={coin}
-              onSelect={c => setCoinName(c.name)}
-              className="mb-8 w-[300px]"
-            />
+            <FieldTitle
+              title="Crypto"
+              description="Choose from supported coin"
+              className="w-[320px] mobile:w-full"
+            >
+              <CoinSelector
+                coins={strategy.supported_pairs}
+                selectedItem={coin}
+                onSelect={c => setCoinName(c.name)}
+              />
+            </FieldTitle>
           )}
+          <div className="grow" />
+
+          <FieldTitle
+            title="Telegram Notification"
+            description="Dont miss a signal by connecting your telegram"
+            className="mobile:w-full"
+          >
+            <Button variant="alternative">
+              <Icon name={bxBell} className="mr-2" />
+              Turn On Notification
+            </Button>
+          </FieldTitle>
         </div>
 
         <div className="mt-10 border-b border-white/5" />
