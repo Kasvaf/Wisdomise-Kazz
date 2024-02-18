e45457633f1cfa95e2fc0cb3897853adc4a1c261
arash16 <arash.shakery@gmail.com>
2024-02-18 17:56:15 +0330

    send all page_view events

--- src/modules/base/routes/Boundary.tsx
+++ src/modules/base/routes/Boundary.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import { ErrorBoundary } from 'react-error-boundary';
+import { analytics } from 'config/segment';
 import PageWrapper from '../PageWrapper';
 import PageError from '../PageError';
 
 const Boundary = () => {
+  const location = useLocation();
+  useEffect(() => {
+    void analytics.pageView(location.pathname + location.search);
+  }, [location]);
+
   return (
     <React.Suspense fallback={<PageWrapper loading />}>
       <ErrorBoundary fallback={<PageError />}>
