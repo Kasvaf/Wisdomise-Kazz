bc3b6819b8cfe01bb901f8f1191253bc36610b58
arash16 <arash.shakery@gmail.com>
2024-02-21 12:45:41 +0330

    set accept-language on all api-calls

--- src/config/useConfig.ts
+++ src/config/useConfig.ts
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
+import axios from 'axios';
 import * as dayjs from 'dayjs';
 import configAxios from './axios';
 import configDayjs from './dayjs';
@@ -15,9 +16,16 @@ configAnalytics();
 configI18n();
 configDayjs();
 
+let lang = 'en';
+axios.interceptors.request.use(config => {
+  config.headers.set('Accept-Language', lang);
+  return config;
+}, null);
+
 export default function useConfig() {
   const { i18n } = useTranslation();
   useEffect(() => {
     dayjs.locale(i18n.language);
+    lang = i18n.language;
   }, [i18n]);
 }
