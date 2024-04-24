53ecabe8a60f1cc8213ead65abf91357a758386f
arash16 <arash.shakery@gmail.com>
2024-04-24 13:06:35 +0330

    fix min-max for mobile

--- src/modules/investment/ButtonFPActivate/useModalFpActivation/MinMaxInfo.tsx
+++ src/modules/investment/ButtonFPActivate/useModalFpActivation/MinMaxInfo.tsx
@@ -6,16 +6,16 @@ const MinMaxInfo: React.FC<{
 }> = ({ min, max }) => {
   return (
     <section className="my-3 flex items-center justify-between border-y border-white/10 py-3">
-      <div className="flex items-center">
-        <span className="text-white/40">Min Investment</span>
-        <span className="ml-3">
+      <div className="flex items-center mobile:flex-col mobile:items-start">
+        <span className="text-white/40 mobile:text-xxs">Min Investment</span>
+        <span className="ml-3 mobile:ml-0">
           <span>{numerable.format(min, '0,0')}</span>
           <span className="ml-1 text-xs text-white/40">USDT</span>
         </span>
       </div>
-      <div className="flex items-center">
-        <span className="text-white/40">Max Investment</span>
-        <span className="ml-3">
+      <div className="flex items-center mobile:flex-col mobile:items-end">
+        <span className="text-white/40 mobile:text-xxs">Max Investment</span>
+        <span className="ml-3 mobile:ml-0">
           <span>{numerable.format(max, '0,0')}</span>
           <span className="ml-1 text-xs text-white/40">USDT</span>
         </span>
