2351af54437c65e18ba013d6df6a268df90a2af9
Amir Hossein Qasemi Moqaddam <amir.qasemi@wisdomise.io>
2024-05-14 12:30:51 +0330

    fix alt coin price

--- src/modules/athena/PageAthena/widgets/altRank/AltRankWidget.tsx
+++ src/modules/athena/PageAthena/widgets/altRank/AltRankWidget.tsx
@@ -1,4 +1,5 @@
 import { clsx } from 'clsx';
+import { roundSensible } from 'utils/numbers';
 import { WidgetWrapper } from '../WidgetWrapper';
 import rankIcon from './rank.svg';
 import { useTopCoinsQuery } from './useTopCoinsQuery';
@@ -19,54 +20,64 @@ export default function AltRankWidget() {
       poweredBy="lunarcrush"
     >
       <section className="mt-6 flex justify-between border-b border-solid border-white/10 pb-4">
-        {theFirstThree?.map(coin => (
-          <div
-            key={coin.asset_id}
-            className="flex shrink grow basis-0 flex-col items-center justify-center gap-4 overflow-hidden"
-          >
-            <img
-              className="h-14 w-14 rounded-lg"
-              src={coin.logo}
-              title={coin.name}
-            />
-            <p className="w-4/5 truncate text-center">{coin.name}</p>
-            <div className="flex items-center gap-2 text-xs">
-              <div className="whitespace-nowrap">
-                {Number(coin.current_price.toFixed(3))} <span>USD</span>
-              </div>
-              <div
-                className={clsx(
-                  'text-[#00FFA3]',
-                  coin.percent_change < 0 && 'text-[#F23645]',
-                )}
-              >
-                {Number(coin.percent_change.toFixed(1))}%
+        {theFirstThree?.map(coin => {
+          const price = roundSensible(coin.current_price);
+
+          return (
+            <div
+              key={coin.asset_id}
+              className="flex shrink grow basis-0 flex-col items-center justify-center gap-4 overflow-hidden"
+            >
+              <img
+                className="h-14 w-14 rounded-lg"
+                src={coin.logo}
+                title={coin.name}
+              />
+              <p className="w-4/5 truncate text-center">{coin.name}</p>
+              <div className="flex items-center gap-2 text-xs">
+                <div className="whitespace-nowrap text-xxs">
+                  {price.toString() === coin.current_price.toString()
+                    ? ''
+                    : '~'}
+                  {roundSensible(coin.current_price)} <span>USD</span>
+                </div>
+                <div
+                  className={clsx(
+                    'text-[#00FFA3]',
+                    coin.percent_change < 0 && 'text-[#F23645]',
+                  )}
+                >
+                  {Number(coin.percent_change.toFixed(1))}%
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </section>
-      {data?.map(coin => (
-        <div
-          key={coin.asset_id}
-          className="flex items-center gap-x-4 border-b border-solid border-white/10 py-4 text-sm"
-        >
-          <img className="w-8 rounded-lg" src={coin.logo} title={coin.name} />
-          <p className="mr-auto text-base">{coin.name}</p>
-          <div className="whitespace-nowrap">
-            {Number(coin.current_price.toFixed(3))}{' '}
-            <span className="text-sm">USD</span>
-          </div>
+      {data?.map(coin => {
+        const price = roundSensible(coin.current_price);
+        return (
           <div
-            className={clsx(
-              'text-sm text-[#00FFA3]',
-              coin.percent_change < 0 && 'text-[#F23645]',
-            )}
+            key={coin.asset_id}
+            className="flex items-center gap-x-4 border-b border-solid border-white/10 py-4 text-sm"
           >
-            {Number(coin.percent_change.toFixed(1))}%
+            <img className="w-8 rounded-lg" src={coin.logo} title={coin.name} />
+            <p className="mr-auto text-base">{coin.name}</p>
+            <div className="whitespace-nowrap">
+              {price.toString() === coin.current_price.toString() ? '' : '~'}
+              {price} <span className="text-sm">USD</span>
+            </div>
+            <div
+              className={clsx(
+                'text-sm text-[#00FFA3]',
+                coin.percent_change < 0 && 'text-[#F23645]',
+              )}
+            >
+              {Number(coin.percent_change.toFixed(1))}%
+            </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </WidgetWrapper>
   );
 }
