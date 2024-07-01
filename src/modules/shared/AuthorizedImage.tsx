01673981c649c5ff62a1825a03d08686531fb510
Amir Momenian <nainemom@gmail.com>
2024-07-01 14:10:15 +0330

    handle AuthorizedImage onError prop

--- src/modules/shared/AuthorizedImage.tsx
+++ src/modules/shared/AuthorizedImage.tsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from 'react';
 import { getJwtToken } from 'modules/auth/jwt-store';
 
-export default function AuthorizedImage(
-  props: React.DetailedHTMLProps<
-    React.ImgHTMLAttributes<HTMLImageElement>,
-    HTMLImageElement
-  >,
-) {
+export default function AuthorizedImage({
+  onError,
+  ...props
+}: React.DetailedHTMLProps<
+  React.ImgHTMLAttributes<HTMLImageElement>,
+  HTMLImageElement
+> & {
+  onError?: () => void;
+}) {
   const { src } = props;
   const [imgSrc, setImgSrc] = useState('');
 
@@ -16,20 +19,24 @@ export default function AuthorizedImage(
         headers: {
           Authorization: `Bearer ${getJwtToken() || ''}`,
         },
-      }).then(async res => {
-        const blob = await res.blob();
-        const reader = new FileReader();
-        await new Promise((resolve, reject) => {
-          reader.addEventListener('load', resolve);
-          // eslint-disable-next-line unicorn/prefer-add-event-listener
-          reader.onerror = reject;
-          reader.readAsDataURL(blob);
+      })
+        .then(async res => {
+          const blob = await res.blob();
+          const reader = new FileReader();
+          await new Promise((resolve, reject) => {
+            reader.addEventListener('load', resolve);
+            // eslint-disable-next-line unicorn/prefer-add-event-listener
+            reader.onerror = reject;
+            reader.readAsDataURL(blob);
+          });
+          reader.result && setImgSrc(reader.result.toString());
+          return null;
+        })
+        .catch(() => {
+          onError?.();
         });
-        reader.result && setImgSrc(reader.result.toString());
-        return null;
-      });
     }
-  }, [src]);
+  }, [src, onError]);
 
   return <img {...props} src={imgSrc} />;
 }
