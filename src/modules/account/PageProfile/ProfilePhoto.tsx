e39cbd70e7cabbd10e42fd01c283ec47cfe90b52
Amir Momenian <nainemom@gmail.com>
2024-07-31 18:03:44 +0330

    fix user profile image

--- src/modules/account/PageProfile/ProfilePhoto.tsx
+++ src/modules/account/PageProfile/ProfilePhoto.tsx
@@ -1,5 +1,6 @@
 import { clsx } from 'clsx';
 import { useState, type FC } from 'react';
+import AuthorizedImage from 'shared/AuthorizedImage';
 import { ProfileCoverPlaceholder, ProfilePicturePlaceholder } from './assets';
 
 export const ProfilePhoto: FC<{
@@ -13,7 +14,7 @@ export const ProfilePhoto: FC<{
   const hasPhoto = typeof src === 'string' && src;
   const trueSrc = hasPhoto && !error ? src : placeholder;
   return (
-    <img
+    <AuthorizedImage
       className={clsx(
         'object-cover object-center',
         type === 'avatar' ? 'bg-white' : 'bg-[#161b34]',
