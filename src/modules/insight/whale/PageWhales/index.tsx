075e462436fb978274b03fb9dc95b239d1dc5acc
Amir Momenian <nainemom@gmail.com>
2024-08-05 14:02:52 +0330

    update whale search

--- src/modules/insight/whale/PageWhales/index.tsx
+++ src/modules/insight/whale/PageWhales/index.tsx
@@ -7,7 +7,6 @@ import { WhalesTable } from './WhalesTable';
 import { CoinsTable } from './CoinsTable';
 import { TopCoins } from './TopCoins';
 import { WalletInput } from './WalletInput';
-import { NetworkSelect } from './NetworkSelect';
 
 export default function PageWhales() {
   const { t } = useTranslation('whale');
@@ -17,15 +16,11 @@ export default function PageWhales() {
     if (!isFetching) setIsLoading(false);
   }, [isFetching]);
 
-  const [networkAbbreviation, setNetworkAbbrevation] = useState<
-    string | undefined
-  >(undefined);
-
   const [walletAddress, setWalletAddress] = useState<string | undefined>(
     undefined,
   );
 
-  const onlyShowWhales = walletAddress || networkAbbreviation;
+  const onlyShowWhales = walletAddress;
 
   return (
     <PageWrapper loading={isLoading} mountWhileLoading>
@@ -40,18 +35,13 @@ export default function PageWhales() {
               className="w-80 mobile:w-full"
               onChange={setWalletAddress}
             />
-            <NetworkSelect
-              valueType="abbreviation"
-              value={networkAbbreviation}
-              onChange={setNetworkAbbrevation}
-            />
           </div>
         </div>
         <div className={clsx('contents', onlyShowWhales && 'hidden')}>
           <CoinsTable />
           <hr className="opacity-5" />
         </div>
-        <WhalesTable />
+        <WhalesTable walletAddress={walletAddress} />
         <div className={clsx('contents', onlyShowWhales && 'hidden')}>
           <hr className="opacity-5" />
           <div className="grid grid-cols-2 gap-8 mobile:grid-cols-1">
