046dbe20f94adbf8af8ae6a41829e8ebd0c28967
Amir Momenian <nainemom@gmail.com>
2024-08-25 17:54:19 +0330

    add feature/flag to insight overview widgets

--- src/modules/insight/PageInsight/index.tsx
+++ src/modules/insight/PageInsight/index.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from 'react-i18next';
 import PageWrapper from 'modules/base/PageWrapper';
 import { PageTitle } from 'shared/PageTitle';
 import { OverviewWidget } from 'shared/OverviewWidget';
+import { useHasFlag } from 'api';
 import { HotCoinsTable } from './HotCoinsTable';
 import { TopWhaleListTable } from './TopWhaleListTable';
 import { TopWhaleCoinsTable } from './TopWhaleCoinsTable';
@@ -11,6 +12,7 @@ import { AlertBox } from './AlertBox';
 
 const PageInsight = () => {
   const { t } = useTranslation();
+  const hasFlag = useHasFlag();
 
   return (
     <PageWrapper>
@@ -21,51 +23,63 @@ const PageInsight = () => {
       />
 
       <div className="grid grid-cols-6 gap-6">
-        <OverviewWidget
-          className="col-span-4 mobile:col-span-full"
-          title={t('coin-radar:hot-coins-section.title')}
-          headerActions={<SeeMoreLink to="/insight/coin-radar" />}
-        >
-          <HotCoinsTable />
-        </OverviewWidget>
-        <OverviewWidget
-          className="col-span-2 bg-gradient-to-t from-v1-background-brand via-v1-background-brand/30 to-v1-surface-l3 mobile:order-first mobile:col-span-full mobile:h-72"
-          contentClassName="h-full"
-        >
-          <AlertBox />
-        </OverviewWidget>
-        <OverviewWidget
-          className="col-span-2 max-h-[470px] mobile:col-span-full"
-          title={t('whale:sections.top-coins.title')}
-          info={t('whale:sections.top-coins.subtitle')}
-          headerActions={<SeeMoreLink to="/insight/whales" />}
-        >
-          <TopWhaleCoinsTable />
-        </OverviewWidget>
-        <OverviewWidget
-          className="col-span-4 mobile:col-span-full"
-          title={t('whale:sections.top-whales.title')}
-          info={t('whale:sections.top-whales.subtitle')}
-          headerActions={<SeeMoreLink to="/insight/whales" />}
-        >
-          <TopWhaleListTable />
-        </OverviewWidget>
-        <OverviewWidget
-          className="col-span-3 max-h-[470px] mobile:col-span-full "
-          title={t('market-pulse:indicator_list.rsi.oversold-full-title')}
-          info={t('market-pulse:indicator_list.rsi.oversold-info')}
-          headerActions={<SeeMoreLink to="/insight/market-pulse" />}
-        >
-          <RsiOvernessTable type="over_sold" />
-        </OverviewWidget>
-        <OverviewWidget
-          className="col-span-3 max-h-[470px] mobile:col-span-full "
-          title={t('market-pulse:indicator_list.rsi.overbought-full-title')}
-          info={t('market-pulse:indicator_list.rsi.overbought-info')}
-          headerActions={<SeeMoreLink to="/insight/market-pulse" />}
-        >
-          <RsiOvernessTable type="over_bought" />
-        </OverviewWidget>
+        {hasFlag('/insight/coin-radar') && (
+          <OverviewWidget
+            className="col-span-4 mobile:col-span-full"
+            title={t('coin-radar:hot-coins-section.title')}
+            headerActions={<SeeMoreLink to="/insight/coin-radar" />}
+          >
+            <HotCoinsTable />
+          </OverviewWidget>
+        )}
+        {hasFlag('/insight/coin-radar') && (
+          <OverviewWidget
+            className="col-span-2 bg-gradient-to-t from-v1-background-brand via-v1-background-brand/30 to-v1-surface-l3 mobile:order-first mobile:col-span-full mobile:h-72"
+            contentClassName="h-full"
+          >
+            <AlertBox />
+          </OverviewWidget>
+        )}
+        {hasFlag('/insight/whales') && (
+          <OverviewWidget
+            className="col-span-2 max-h-[470px] mobile:col-span-full"
+            title={t('whale:sections.top-coins.title')}
+            info={t('whale:sections.top-coins.subtitle')}
+            headerActions={<SeeMoreLink to="/insight/whales" />}
+          >
+            <TopWhaleCoinsTable />
+          </OverviewWidget>
+        )}
+        {hasFlag('/insight/whales') && (
+          <OverviewWidget
+            className="col-span-4 mobile:col-span-full"
+            title={t('whale:sections.top-whales.title')}
+            info={t('whale:sections.top-whales.subtitle')}
+            headerActions={<SeeMoreLink to="/insight/whales" />}
+          >
+            <TopWhaleListTable />
+          </OverviewWidget>
+        )}
+        {hasFlag('/insight/market-pulse') && (
+          <OverviewWidget
+            className="col-span-3 max-h-[470px] mobile:col-span-full "
+            title={t('market-pulse:indicator_list.rsi.oversold-full-title')}
+            info={t('market-pulse:indicator_list.rsi.oversold-info')}
+            headerActions={<SeeMoreLink to="/insight/market-pulse" />}
+          >
+            <RsiOvernessTable type="over_sold" />
+          </OverviewWidget>
+        )}
+        {hasFlag('/insight/market-pulse') && (
+          <OverviewWidget
+            className="col-span-3 max-h-[470px] mobile:col-span-full "
+            title={t('market-pulse:indicator_list.rsi.overbought-full-title')}
+            info={t('market-pulse:indicator_list.rsi.overbought-info')}
+            headerActions={<SeeMoreLink to="/insight/market-pulse" />}
+          >
+            <RsiOvernessTable type="over_bought" />
+          </OverviewWidget>
+        )}
       </div>
     </PageWrapper>
   );
