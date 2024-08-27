8f996be4049954ab5d50dc15010c8282158432f1
arash16 <arash.shakery@gmail.com>
2024-08-27 12:20:25 +0330

    menu page only for mobile

--- src/modules/base/Container/SideMenu/PageMenu.tsx
+++ src/modules/base/Container/SideMenu/PageMenu.tsx
@@ -1,7 +1,18 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import useIsMobile from 'utils/useIsMobile';
 import WalletDropdownContent from '../Header/WalletDropdown/WalletDropdownContent';
 import MenuItemsContent from './MenuItemsContent';
 
 const PageMenu = () => {
+  const isMobile = useIsMobile();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!isMobile) {
+      navigate('/');
+    }
+  }, [isMobile, navigate]);
+
   return (
     <div>
       <div className="mb-4 overflow-hidden rounded-xl bg-black/30">
