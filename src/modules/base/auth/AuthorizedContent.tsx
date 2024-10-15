603e862cc9da8736793dfba9e1173ac2ff0f86f0
arash16 <arash.shakery@gmail.com>
2024-10-15 09:10:05 +0330

    restrict unauthorized pages

---
+++ src/modules/base/auth/AuthorizedContent.tsx
@@ -0,0 +1,35 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useEffect, type PropsWithChildren } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useHasFlag } from 'api';
+import Button from 'shared/Button';
+import useEnsureAuthenticated from 'shared/useEnsureAuthenticated';
+import { useIsLoggedIn } from './jwt-store';
+
+const AuthorizedContent: React.FC<PropsWithChildren> = ({ children }) => {
+  const { t } = useTranslation('auth');
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+  const hasFlag = useHasFlag();
+  const isAuthorized = hasFlag(pathname);
+  const isLoggedIn = useIsLoggedIn();
+  const [ModalLogin, ensureAuthenticated] = useEnsureAuthenticated();
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate('/');
+    }
+  }, [isLoggedIn, navigate]);
+
+  return isAuthorized ? (
+    <>{children}</>
+  ) : (
+    <div className="mt-12 flex h-full flex-col items-center justify-center gap-4 text-v1-content-primary">
+      <div>{t('error-not-authorized')}</div>
+      <Button onClick={ensureAuthenticated}>{t('base:user.sign-in')}</Button>
+      {ModalLogin}
+    </div>
+  );
+};
+
+export default AuthorizedContent;
