35d3c9b5bd2d2797992e34c5cc43fc677bf29ba8
Majid Pouramini <pouramini.majid@gmail.com>
2024-10-24 14:58:20 +0330

    separate miniapp dev command

--- src/utils/version.ts
+++ src/utils/version.ts
@@ -3,4 +3,6 @@ export const isLocal = /^([\w-]+\.)*localhost$/.test(window.location.hostname);
 export const isProduction =
   !window.location.hostname.startsWith('stage-') && !isLocal;
 
-export const isMiniApp = window.location.hostname.includes('tg-app');
+export const isMiniApp =
+  (isLocal && import.meta.env.VITE_IS_MINIAPP) ||
+  window.location.hostname.includes('tg-app');
