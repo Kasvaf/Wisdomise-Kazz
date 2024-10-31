9e1b406241de0a284fe2fab2b5fbbebac2baeee4
arash16 <arash.shakery@gmail.com>
2024-10-31 14:05:12 +0330

    hotfix: add label for open-order field

--- src/modules/builder/signaler/PageSignalerDetails/TabTerminal/AdvancedSignalForm/PartOpen.tsx
+++ src/modules/builder/signaler/PageSignalerDetails/TabTerminal/AdvancedSignalForm/PartOpen.tsx
@@ -96,6 +96,7 @@ const PartOpen: React.FC<{
       <div className="my-4 border-b border-white/5" />
 
       <PriceVolumeInput
+        label="Open"
         price={
           orderType === 'market' && !isUpdate
             ? assetPrice === undefined
