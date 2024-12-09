8d2c3364756e059828e965aeb94e0ef658e8d385
Amir Momenian <nainemom@gmail.com>
2024-12-09 18:22:04 +0330

    update position normalizer

--- src/modules/insight/PageMarketPulse/components/TechnicalOverviewWidget/TechnicalCharts/useNormalizeTechnicalChartBubbles.tsx
+++ src/modules/insight/PageMarketPulse/components/TechnicalOverviewWidget/TechnicalCharts/useNormalizeTechnicalChartBubbles.tsx
@@ -6,6 +6,8 @@ export const useNormalizeTechnicalChartBubbles = (
   type: 'cheap_bullish' | 'expensive_bearish',
 ) =>
   useMemo(() => {
+    const usedXs = new Set<string>();
+    const usedYs = new Set<string>();
     const parsedData = data
       .filter(x => {
         const isScoreMatched =
@@ -20,12 +22,23 @@ export const useNormalizeTechnicalChartBubbles = (
           typeof x.macd_score === 'number'
         );
       })
+      .sort((a, b) => Math.abs(b.rsi_score ?? 0) - Math.abs(a.rsi_score ?? 0))
+      .sort((a, b) => Math.abs(b.macd_score ?? 0) - Math.abs(a.macd_score ?? 0))
       .sort((a, b) => Math.abs(b.score ?? 0) - Math.abs(a.score ?? 0))
       .slice(0, 10)
       .map(raw => {
-        const x = Math.abs(raw.macd_score ?? 0);
-        const y = Math.abs(raw.rsi_score ?? 0);
+        let x = Math.abs(raw.macd_score ?? 0);
+        let y = Math.abs(raw.rsi_score ?? 0);
         const size = Math.abs(raw.data?.price_change_percentage_24h ?? 0);
+
+        while (usedXs.has(x.toFixed(1))) {
+          x += 0.25;
+        }
+        while (usedYs.has(y.toFixed(1))) {
+          y += 0.25;
+        }
+        usedXs.add(x.toFixed(1));
+        usedYs.add(y.toFixed(1));
 
         const color =
           (raw.data?.price_change_percentage_24h ?? 0) > 0
