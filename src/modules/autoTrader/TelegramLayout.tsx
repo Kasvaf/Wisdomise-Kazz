528abfc7c778e3bb7edf54f3334b11a9991b03cc
arash16 <arash.shakery@gmail.com>
2024-12-12 14:19:38 +0330

    initial user profile in header

--- src/modules/autoTrader/TelegramLayout.tsx
+++ src/modules/autoTrader/TelegramLayout.tsx
@@ -1,11 +1,44 @@
 import React, { useRef } from 'react';
-import { Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 import { TonConnectButton } from '@tonconnect/ui-react';
 import OnboardingMessageProvider from 'shared/Onboarding/OnboardingMessageProvider';
 import PageWrapper from 'modules/base/PageWrapper';
 import BottomNavBar from 'modules/base/Container/BottomNavBar';
 import ScrollToTop from 'modules/base/Container/ScrollToTop';
 import Logo from 'assets/logo.png';
+import { useTelegramProfile } from './TelegramProvider';
+
+const ProfileInfo = () => {
+  const profile = useTelegramProfile();
+
+  if (!profile?.username) {
+    return <img src={Logo} className="h-8" alt="logo" />;
+  }
+
+  return (
+    <NavLink
+      className="flex max-w-[180px] items-center rounded-lg pr-4 hover:bg-black/20"
+      to="/trader-claim-reward"
+    >
+      {profile?.photo_url ? (
+        <img src={profile?.photo_url} className="mr-2 size-[40px] rounded-lg" />
+      ) : (
+        <div className="mr-2 flex size-[40px] items-center justify-center rounded-lg bg-v1-background-brand">
+          {(profile?.first_name ?? profile.username)[0].toUpperCase()}
+        </div>
+      )}
+
+      <div>
+        <div className="text-sm">
+          {profile?.first_name} {profile?.last_name}
+        </div>
+        <div className="text-xs text-v1-content-secondary">
+          {profile.username}
+        </div>
+      </div>
+    </NavLink>
+  );
+};
 
 export function TelegramLayout() {
   const mainRef = useRef<HTMLDivElement>(null);
@@ -14,7 +47,7 @@ export function TelegramLayout() {
     <main className="relative bg-page">
       <OnboardingMessageProvider>
         <div className="fixed end-0 start-0 top-0 z-10 flex items-center justify-between bg-page p-4">
-          <img src={Logo} className="h-8" alt="logo" />
+          <ProfileInfo />
           <TonConnectButton />
         </div>
         <div
