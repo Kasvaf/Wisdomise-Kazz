f8b625bb95ea29a87071076e074c5963ebc4330a
Amir Momenian <nainemom@gmail.com>
2024-12-17 20:04:48 +0330

    update gtag method

--- src/config/gtag.ts
+++ src/config/gtag.ts
@@ -4,6 +4,11 @@ export const gtag = (
   type: string,
   event: string,
   parameters: Record<string, string | number | boolean> = {},
+) => {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment, @typescript-eslint/prefer-ts-expect-error
   /* @ts-ignore */
-) => window?.gtag?.(type, event, parameters);
+  return window?.gtag?.({
+    [type]: event,
+    ...parameters,
+  });
+};
