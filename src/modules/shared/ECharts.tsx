67bd364393b1bd312bd9c049e6ce11471b4e0377
Amir Momenian <nainemom@gmail.com>
2024-12-24 23:52:13 +0330

    fix echarts component updates

--- src/modules/shared/ECharts.tsx
+++ src/modules/shared/ECharts.tsx
@@ -11,39 +11,45 @@ import { type FC, useEffect, useRef } from 'react';
 
 export const ECharts: FC<{
   className?: string;
-  theme?: string;
   options: EChartsOption;
   initOptions?: EChartsInitOpts;
   // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
   onClick?: (e: ECElementEvent) => boolean | void;
-}> = ({ className, initOptions, theme, options: userOptions, onClick }) => {
+}> = ({ className, initOptions, options: userOptions, onClick }) => {
   const element = useRef<HTMLDivElement>(null);
   const chart = useRef<EChartsType | undefined>(undefined);
 
   // initialize
   useEffect(() => {
-    if (!element.current) return;
+    if (!element.current || chart.current) return;
 
-    if (!chart.current) {
-      chart.current = init(element.current, theme ?? 'dark', initOptions);
-    }
-    return () => {
-      chart.current?.clear();
-    };
-  }, [theme, initOptions, userOptions]);
+    chart.current = init(element.current, 'dark', initOptions);
+    chart.current.setOption(
+      {
+        tooltip: {
+          backgroundColor: '#282a32',
+          textStyle: {
+            color: '#ffffff',
+          },
+        },
+      },
+      true,
+    );
+  }, [initOptions]);
 
   // sync options
   useEffect(() => {
     if (!chart.current) return;
-    if (onClick) chart.current.on('click', onClick);
-    chart.current.setOption(userOptions);
+    const handleClick = onClick ?? (() => true);
+    chart.current.on('click', handleClick);
+    chart.current.setOption(userOptions, false);
     return () => {
       if (!chart.current) return;
-      if (onClick) chart.current.off('click', onClick);
+      chart.current.off('click', handleClick);
     };
   }, [userOptions, onClick]);
 
-  // runtime effects
+  // dynamic resize
   useEffect(() => {
     if (!chart.current) return;
     let resizeTimeout: ReturnType<typeof setTimeout> | null = null;
@@ -64,5 +70,5 @@ export const ECharts: FC<{
     };
   });
 
-  return <div ref={element} className={className}></div>;
+  return <div ref={element} className={className} />;
 };
