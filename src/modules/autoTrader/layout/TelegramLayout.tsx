341933616912405846ee091160cd079b56b6ab14
Majid Pouramini <pouramini.majid@gmail.com>
2025-01-08 17:43:28 +0330

    add trade and connect wallet events

--- src/modules/autoTrader/layout/TelegramLayout.tsx
+++ src/modules/autoTrader/layout/TelegramLayout.tsx
@@ -1,11 +1,12 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
-import { TonConnectButton } from '@tonconnect/ui-react';
+import { TonConnectButton, useTonAddress } from '@tonconnect/ui-react';
 import OnboardingMessageProvider from 'shared/Onboarding/OnboardingMessageProvider';
 import PageWrapper from 'modules/base/PageWrapper';
 import BottomNavBar from 'modules/base/Container/BottomNavBar';
 import ScrollToTop from 'modules/base/Container/ScrollToTop';
 import Logo from 'assets/logo.png';
+import { gtag } from 'config/gtag';
 import { useTelegramProfile } from './TelegramProvider';
 import FabSupport from './FabSupport';
 import box from './box.png';
@@ -65,6 +66,13 @@ const ProfileInfo = () => {
 
 export default function TelegramLayout() {
   const mainRef = useRef<HTMLDivElement>(null);
+  const address = useTonAddress();
+
+  useEffect(() => {
+    if (address) {
+      gtag('event', 'connect_wallet');
+    }
+  }, [address]);
 
   return (
     <main className="relative bg-page">
