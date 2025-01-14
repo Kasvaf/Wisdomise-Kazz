40b7aad4cf57f9c0f721ee8f6b2996f4304b7863
Amir Momenian <nainemom@gmail.com>
2025-01-14 13:25:47 +0330

    create design system input component

---
+++ src/modules/shared/v1-components/Input.tsx
@@ -0,0 +1,85 @@
+import { clsx } from 'clsx';
+import {
+  type ChangeEventHandler,
+  useCallback,
+  type ReactNode,
+  useRef,
+} from 'react';
+
+export function Input<T extends 'number' | 'string'>({
+  size = 'xl',
+  value,
+  onChange,
+  type,
+  disabled,
+  loading,
+  block,
+  placeholder,
+  className,
+  prefixIcon,
+  suffixIcon,
+}: {
+  size?: 'xs' | 'sm' | 'md' | 'xl';
+
+  type?: T;
+
+  value?: T extends 'number' ? number : string;
+  onChange?: (
+    newValue: T extends 'number' ? number | undefined : string,
+  ) => void;
+
+  disabled?: boolean;
+  loading?: boolean;
+  block?: boolean;
+  placeholder?: string;
+  prefixIcon?: ReactNode;
+  suffixIcon?: ReactNode;
+  className?: string;
+}) {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = useCallback(
+    e => {
+      if (type === 'number') {
+        onChange?.((e.target.valueAsNumber ?? undefined) as never);
+      } else {
+        onChange?.((e.target.value ?? '') as never);
+      }
+    },
+    [onChange, type],
+  );
+  return (
+    <div
+      className={clsx(
+        /* Size: height, padding, font-size, border-radius */
+        size === 'xs' && 'h-xs rounded-md px-3 text-xs',
+        size === 'sm' && 'h-sm rounded-lg px-3 text-xs',
+        size === 'md' && 'h-md rounded-lg px-3 text-xs',
+        size === 'xl' && 'h-xl rounded-xl px-4 text-sm',
+        /* Loading */
+        loading && 'animate-pulse',
+        /* Disabled */
+        'aria-disabled::cursor-not-allowed aria-disabled:border-transparent aria-disabled:bg-white/5 aria-disabled:bg-none aria-disabled:text-white/50 aria-disabled:grayscale',
+        /* Shared */
+        'bg-v1-surface-l-next',
+        'border border-transparent font-normal transition-all focus-within:border-v1-border-focus [&_svg]:size-5',
+        block ? 'flex' : 'inline-flex',
+        'items-center justify-between gap-1',
+        className,
+      )}
+      aria-disabled={disabled}
+      onClick={() => inputRef.current?.focus()}
+    >
+      {prefixIcon}
+      <input
+        className="block w-full grow border-0 bg-transparent text-inherit outline-none"
+        value={value}
+        onChange={handleChange}
+        disabled={disabled}
+        placeholder={placeholder}
+        ref={inputRef}
+      />
+      {suffixIcon}
+    </div>
+  );
+}
