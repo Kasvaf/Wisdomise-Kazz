6d562f350a9efd7be0a0ec697ff27d9103af823e
arash16 <arash.shakery@gmail.com>
2025-01-14 10:33:05 +0330

    mini-app web sync login: initial mini-app side

---
+++ src/modules/autoTrader/useStartParams.ts
@@ -0,0 +1,16 @@
+import { useMemo } from 'react';
+import { isLocal } from 'utils/version';
+import { useTelegram } from './layout/TelegramProvider';
+
+const useStartParams = () => {
+  const { webApp } = useTelegram();
+  return useMemo(() => {
+    const initData = isLocal
+      ? import.meta.env.VITE_CUSTOM_QUERY
+      : webApp?.initData;
+    const startParam = new URLSearchParams(initData).get('start_param');
+    return startParam?.split('_') ?? [];
+  }, [webApp?.initData]);
+};
+
+export default useStartParams;
