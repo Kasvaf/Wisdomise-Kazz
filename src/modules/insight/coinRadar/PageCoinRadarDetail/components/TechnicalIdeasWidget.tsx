570a180577c22a786c5d4b7d42e8324fd37c7303
arash16 <arash.shakery@gmail.com>
2025-01-14 23:37:23 +0330

    auto-trader: add chart to coin detail

--- src/modules/insight/coinRadar/PageCoinRadarDetail/components/TechnicalIdeasWidget.tsx
+++ src/modules/insight/coinRadar/PageCoinRadarDetail/components/TechnicalIdeasWidget.tsx
@@ -1,4 +1,3 @@
-import { AdvancedRealTimeChart } from 'react-ts-tradingview-widgets';
 import { useTranslation } from 'react-i18next';
 import { Fragment, useMemo, useState } from 'react';
 import { bxChevronDown, bxLinkExternal } from 'boxicons-quasar';
@@ -7,6 +6,7 @@ import { OverviewWidget } from 'shared/OverviewWidget';
 import Icon from 'shared/Icon';
 import useIsMobile from 'utils/useIsMobile';
 import { SocialMessageSummary } from './CoinSocialFeedWidget/SocialMessage';
+import CoinChart from './CoinChart';
 
 export function TechnicalIdeasWidget({
   slug,
@@ -77,32 +77,10 @@ export function TechnicalIdeasWidget({
     >
       {hasChart && (
         <div className="h-[500px] overflow-hidden rounded-xl bg-v1-surface-l3 p-2 mobile:h-[350px]">
-          {coinOverview.data?.charts_id?.trading_view_chart_id ? (
-            <AdvancedRealTimeChart
-              allow_symbol_change={false}
-              symbol={coinOverview.data.charts_id.trading_view_chart_id}
-              style="1"
-              interval="60"
-              hide_side_toolbar
-              hotlist={false}
-              theme="dark"
-              height={isMobile ? 340 : 490}
-              width="100%"
-            />
-          ) : coinOverview.data?.charts_id?.gecko_terminal_chart_id ? (
-            <iframe
-              height="100%"
-              width="100%"
-              id="geckoterminal-embed"
-              title="GeckoTerminal Embed"
-              src={`https://www.geckoterminal.com/${coinOverview.data.charts_id.gecko_terminal_chart_id}?embed=1&info=0&swaps=0&grayscale=0&light_chart=0`}
-              frameBorder="0"
-              allow="clipboard-write"
-              allowFullScreen
-            />
-          ) : null}
+          <CoinChart slug={slug} height={isMobile ? 340 : 490} />
         </div>
       )}
+
       {tradingViewMessages.length > 0 && (
         <div className="space-y-4 overflow-x-auto rounded-xl bg-v1-surface-l3 p-6 mobile:bg-transparent mobile:p-0">
           {tradingViewMessages.slice(0, limit).map((msg, idx, self) => (
