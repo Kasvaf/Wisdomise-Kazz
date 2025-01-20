584c803759ca191691a0cdf9ac4952383ca68ecf
Amir Momenian <nainemom@gmail.com>
2025-01-20 12:19:29 +0330

    add loading state for coin radar overview

--- src/modules/insight/PageInsight/index.tsx
+++ src/modules/insight/PageInsight/index.tsx
@@ -1,6 +1,7 @@
 import { Trans, useTranslation } from 'react-i18next';
 import PageWrapper from 'modules/base/PageWrapper';
 import { PageTitle } from 'shared/PageTitle';
+import { useCoinRadarCoins } from 'api';
 import { PageInsightMeta } from './components/PageInsightMeta';
 import { InsightOnboarding } from './components/InsightOnboarding';
 import { InsightAlertButton } from './components/InsightAlertButton';
@@ -9,9 +10,10 @@ import { CoinRadarTable } from './components/CoinRadarTable';
 
 const PageInsight = () => {
   const { t } = useTranslation();
+  const coins = useCoinRadarCoins();
 
   return (
-    <PageWrapper>
+    <PageWrapper loading={coins.isLoading}>
       <PageInsightMeta />
       <div className="flex flex-col gap-6">
         <div className="flex items-center justify-between gap-6">
