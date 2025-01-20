caf1dae112b494269f145e3cbe8e1ad49a43a70e
Amir Momenian <nainemom@gmail.com>
2025-01-20 09:45:45 +0330

    remove social radar feedback event

--- src/modules/insight/coinRadar/PageCoinRadar/index.tsx
+++ src/modules/insight/coinRadar/PageCoinRadar/index.tsx
@@ -1,14 +1,8 @@
-import { useEffectOnce } from 'usehooks-ts';
 import PageWrapper from 'modules/base/PageWrapper';
-import { track } from 'config/segment';
 import { CoinRadarOnboarding } from './components/CoinRadarOnboarding';
 import { HotCoinsWidget } from './components/HotCoinsWidget';
 
 export default function PageCoinRadar() {
-  useEffectOnce(() => {
-    track('Feedback Coin Radar');
-  });
-
   return (
     <PageWrapper>
       <HotCoinsWidget />
