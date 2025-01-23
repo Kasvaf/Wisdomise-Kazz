33f58e66756f1a47651f7128a348f263eb6a1446
Amir Momenian <nainemom@gmail.com>
2025-01-23 23:00:22 +0330

    add steps to onboarding page

--- src/modules/insight/PageOnboarding/index.tsx
+++ src/modules/insight/PageOnboarding/index.tsx
@@ -1,3 +1,72 @@
+import { type ComponentProps, useMemo, useState } from 'react';
+import { useUserStorage } from 'api/userStorage';
+import { OnboardingView } from './components/OnboardingView';
+import { ApprochStep } from './components/steps/ApprochStep';
+import { ExperienceStep } from './components/steps/ExperienceStep';
+import { FeaturesStep } from './components/steps/FeaturesStep';
+import { ReadyStep } from './components/steps/ReadyStep';
+import { useShowOnboardingIfNeeded } from './hooks/useShowOnboardingIfNeeded';
+
 export default function PageOnboarding() {
-  return <>...</>;
+  const done = useShowOnboardingIfNeeded();
+  const [step, setStep] = useState('approch');
+  const { value: experience, save: setExperience } =
+    useUserStorage('experience');
+  const { value: approch, save: setApproch } = useUserStorage('approch');
+
+  const steps = useMemo<ComponentProps<typeof OnboardingView>['steps']>(
+    () => [
+      {
+        key: 'experience',
+        label: 'Experience & Goals',
+        element: (
+          <ExperienceStep
+            onNext={newValue => {
+              void setExperience(newValue);
+              setStep('approch');
+            }}
+            value={experience ?? undefined}
+          />
+        ),
+      },
+      {
+        key: 'approch',
+        label: 'Trading Approach',
+        element: (
+          <ApprochStep
+            value={approch ? JSON.parse(approch) : []}
+            onPrev={() => setStep('experience')}
+            onNext={newValue => {
+              void setApproch(JSON.stringify(newValue));
+              setStep('features');
+            }}
+          />
+        ),
+      },
+      {
+        key: 'features',
+        label: 'Features',
+        element: (
+          <FeaturesStep
+            onPrev={() => setStep('approch')}
+            onNext={() => setStep('ready')}
+          />
+        ),
+      },
+      {
+        key: 'ready',
+        label: 'Start Your Journey',
+        element: (
+          <ReadyStep
+            value={approch ? JSON.parse(approch) : []}
+            onPrev={() => setStep('features')}
+            onNext={() => done()}
+          />
+        ),
+      },
+    ],
+    [approch, done, experience, setApproch, setExperience],
+  );
+
+  return <OnboardingView onClose={done} steps={steps} step={step} />;
 }
