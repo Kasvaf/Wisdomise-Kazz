5b294fb5bd288c328dbbebc7259ced3a3068c040
Amir Momenian <nainemom@gmail.com>
2025-01-23 21:25:49 +0330

    create onboarding page root components

---
+++ src/modules/insight/PageOnboarding/components/OnboardingView.tsx
@@ -0,0 +1,96 @@
+import { bxsCheckCircle, bxX } from 'boxicons-quasar';
+import { clsx } from 'clsx';
+import { Fragment, useMemo, type ReactNode } from 'react';
+import Icon from 'shared/Icon';
+import { Button } from 'shared/v1-components/Button';
+
+export function OnboardingView<V extends string>({
+  className,
+  onClose,
+  steps: rawSteps,
+  step,
+}: {
+  className?: string;
+  onClose?: () => void;
+  steps: Array<{
+    key: V;
+    label: ReactNode;
+    element: ReactNode;
+  }>;
+  step: V;
+}) {
+  const steps = useMemo(() => {
+    const activeStepIndex = rawSteps.findIndex(x => x.key === step);
+    return rawSteps.map((x, index) => ({
+      ...x,
+      isPassed: index < activeStepIndex,
+      isActive: index === activeStepIndex,
+    }));
+  }, [rawSteps, step]);
+
+  return (
+    <div
+      className={clsx(
+        'fixed left-0 top-0 flex h-dvh w-dvw bg-v1-surface-l1 text-v1-content-primary',
+        className,
+      )}
+    >
+      <div className="absolute -top-1/3 left-1/2 !size-[520px] -translate-x-1/2 bg-white/25 blur-[200px] mobile:!size-[430px]" />
+      <div className="relative mx-auto flex w-full max-w-7xl grow flex-col">
+        <div className="flex shrink-0 items-center justify-end px-4 pt-4">
+          <Button
+            variant="ghost"
+            onClick={onClose}
+            className="w-md shrink-0"
+            size="md"
+          >
+            <Icon name={bxX} />
+          </Button>
+        </div>
+
+        <div
+          className={clsx(
+            'relative flex w-full shrink-0 flex-nowrap items-start justify-between gap-2 p-4 xl:pt-8',
+            className,
+          )}
+        >
+          {steps.map(x => (
+            <div
+              key={x.key}
+              className={clsx(
+                'flex shrink basis-full cursor-default flex-col items-start gap-3 transition-all',
+              )}
+            >
+              <div
+                className={clsx(
+                  'h-2 w-full rounded-full transition-all',
+                  x.isPassed || x.isActive ? 'bg-white' : 'bg-white/10',
+                )}
+              />
+              <div
+                className={clsx(
+                  'w-4/5 rounded-full text-sm font-medium mobile:text-xs',
+                  x.isPassed || x.isActive ? 'text-white' : 'text-white/50',
+                )}
+              >
+                {x.isPassed ? (
+                  <Icon name={bxsCheckCircle} size={20} />
+                ) : (
+                  x.label
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="relative flex grow flex-col overflow-hidden">
+          {steps
+            .filter(x => x.key === step)
+            .map(x => (
+              <Fragment key={x.key}>{x.element}</Fragment>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+}
