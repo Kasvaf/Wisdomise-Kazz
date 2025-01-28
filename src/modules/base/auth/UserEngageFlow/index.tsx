bd61ded3fa12bb71eb691fc1ac079ca2b6420e1b
Amir Momenian <nainemom@gmail.com>
2025-01-28 14:40:42 +0330

    remove trial started modal

--- src/modules/base/auth/UserEngageFlow/index.tsx
+++ src/modules/base/auth/UserEngageFlow/index.tsx
@@ -1,42 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useTimeout } from 'usehooks-ts';
 import { useSubscription } from 'api';
 import { useUserStorage } from 'api/userStorage';
 import { useEmbedView } from 'modules/embedded/useEmbedView';
 import { useIsLoggedIn } from '../jwt-store';
-import { TrialStartedModal } from './TrialStartedModal';
 import { SemiForceLoginModal } from './SemiForceLoginModal';
 
 const useSemiForceLoginModal = () => {
   const isLoggedIn = useIsLoggedIn();
   return !isLoggedIn;
-};
-
-const useTrialStartedModal = () => {
-  const { group } = useSubscription();
-  const [isDismissed, setIsDismissed] = useState(false);
-  const userStorage = useUserStorage('trial-popup');
-  const [isReady, setIsReady] = useState(false);
-
-  useTimeout(() => {
-    setIsReady(true);
-  }, 1000);
-
-  const trialStartedModal =
-    group === 'trial' &&
-    userStorage.value !== 'true' &&
-    !userStorage.isLoading &&
-    !isDismissed &&
-    isReady;
-
-  return [
-    trialStartedModal,
-    () => {
-      setIsDismissed(false);
-      void userStorage.save('true');
-    },
-  ] as const;
 };
 
 const ONBOARDING_URL = '/coin-radar/onboarding';
@@ -59,18 +31,11 @@ const useOnboarding = () => {
 export function UserEngageFlow() {
   const { isEmbeddedView } = useEmbedView();
   const loginModal = useSemiForceLoginModal();
-  const [trialStartedModal, dismissTrialStartedModal] = useTrialStartedModal();
   useOnboarding();
 
   return isEmbeddedView ? null : (
     <>
       <SemiForceLoginModal open={loginModal} />
-      <TrialStartedModal
-        open={trialStartedModal}
-        onClose={() => {
-          dismissTrialStartedModal();
-        }}
-      />
     </>
   );
 }
