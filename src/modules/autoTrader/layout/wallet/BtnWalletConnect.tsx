369ccc4726ea7fb1c57709a5b9639b61881b5633
arash16 <arash.shakery@gmail.com>
2025-02-02 18:52:34 +0330

    better style for connected wallet

--- src/modules/autoTrader/layout/wallet/BtnWalletConnect.tsx
+++ src/modules/autoTrader/layout/wallet/BtnWalletConnect.tsx
@@ -6,16 +6,21 @@ import '@solana/wallet-adapter-react-ui/styles.css';
 
 const BtnSolanaWalletConnect = () => {
   const solanaWallet = useWallet();
+  const addr = solanaWallet.publicKey?.toString() || '';
   return (
     <WalletMultiButton
       style={{
         borderRadius: 20,
         height: 40,
         fontSize: 14,
-        ...(solanaWallet.connected ? {} : { background: '#00a3ff' }),
+        ...(solanaWallet.connected
+          ? { background: '#121214', paddingLeft: 16 }
+          : { background: '#00a3ff' }),
       }}
     >
-      {solanaWallet.connected ? 'Solana Wallet' : 'Connect Wallet'}
+      {solanaWallet.connected
+        ? addr.substring(0, 4) + '...' + addr.substr(-4)
+        : 'Connect Wallet'}
     </WalletMultiButton>
   );
 };
