4b8dac03f490a445ae71856e06df43fff31ad43c
arash16 <arash.shakery@gmail.com>
2025-02-03 13:59:24 +0330

    patch wallet-adapter-react for telegram web-view

--- src/modules/autoTrader/layout/wallet/BtnWalletConnect.tsx
+++ src/modules/autoTrader/layout/wallet/BtnWalletConnect.tsx
@@ -1,38 +1,33 @@
 import { TonConnectButton } from '@tonconnect/ui-react';
 import { useWallet } from '@solana/wallet-adapter-react';
-import { useWalletModal } from '@solana/wallet-adapter-react-ui';
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 import useActiveNetwork from 'modules/autoTrader/layout/useActiveNetwork';
-import Button from 'shared/Button';
 import '@solana/wallet-adapter-react-ui/styles.css';
 
 const BtnSolanaWalletConnect = () => {
-  const { setVisible } = useWalletModal();
   const solanaWallet = useWallet();
   const addr = solanaWallet.publicKey?.toString() || '';
-
   return (
-    <Button
-      variant={solanaWallet.connected ? 'alternative' : 'brand'}
-      className="h-10 rounded-[20px] !px-5 !py-0"
-      onClick={() => setVisible(true)}
+    <WalletMultiButton
+      style={{
+        borderRadius: 20,
+        height: 40,
+        fontSize: 14,
+        ...(solanaWallet.connected
+          ? { background: '#121214', paddingLeft: 16 }
+          : { background: '#00a3ff' }),
+      }}
     >
-      {solanaWallet.connected ? (
-        <>
-          <img
-            src={solanaWallet.wallet?.adapter.icon}
-            className="mr-2 size-6"
-          />
-          {addr.substring(0, 4) + '...' + addr.substr(-4)}
-        </>
-      ) : (
-        'Connect Wallet'
-      )}
-    </Button>
+      {solanaWallet.connected
+        ? addr.substring(0, 4) + '...' + addr.substr(-4)
+        : 'Connect Wallet'}
+    </WalletMultiButton>
   );
 };
 
 const BtnWalletConnect: React.FC = () => {
   const net = useActiveNetwork();
+
   return net === 'the-open-network' ? (
     <TonConnectButton />
   ) : net === 'solana' ? (
