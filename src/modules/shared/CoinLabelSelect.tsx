a3ae7a36c17cbd31965e36e6347ca458a24496dc
Amir Momenian <nainemom@gmail.com>
2025-02-04 18:43:26 +0330

    create coin label select component

---
+++ src/modules/shared/CoinLabelSelect.tsx
@@ -0,0 +1,44 @@
+import { type ComponentProps } from 'react';
+import { clsx } from 'clsx';
+import { useTranslation } from 'react-i18next';
+import { type CoinLabels, useCoinLabels } from 'api';
+import { Select } from 'shared/v1-components/Select';
+import { CoinLabel } from 'shared/CoinLabels';
+
+export function CoinLabelSelect<M extends boolean>({
+  type,
+  ...props
+}: Omit<
+  ComponentProps<typeof Select<string, M>>,
+  | 'render'
+  | 'onSearch'
+  | 'options'
+  | 'loading'
+  | 'showSearch'
+  | 'searchValue'
+  | 'onSearch'
+> & {
+  type: keyof CoinLabels;
+}) {
+  const { t } = useTranslation('coin-radar');
+  const options = useCoinLabels();
+
+  return (
+    <Select
+      loading={options.isLoading}
+      render={(val, target) => {
+        if (!val) return t('common.all_labels');
+        return (
+          <CoinLabel
+            value={val}
+            type={type}
+            popup={false}
+            className={clsx(target === 'value' && '!bg-transparent !px-0')}
+          />
+        );
+      }}
+      options={options.data?.[type]?.filter(x => !!x) ?? []}
+      {...props}
+    />
+  );
+}
