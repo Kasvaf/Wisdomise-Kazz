7da40eff08d499d637d1419dc85c1c9babc2cd3d
Amir Momenian <nainemom@gmail.com>
2025-02-11 01:53:33 +0330

    improve user storage api

--- src/api/userStorage.ts
+++ src/api/userStorage.ts
@@ -2,51 +2,44 @@ import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { useEffect, useState } from 'react';
 import { ACCOUNT_PANEL_ORIGIN } from 'config/constants';
 import { ofetch } from 'config/ofetch';
-import { useIsLoggedIn } from 'modules/base/auth/jwt-store';
-import { useAccountQuery } from './account';
+import { useJwtEmail } from 'modules/base/auth/jwt-store';
+
+const undefinedSymbol = Symbol('undefined');
 
 export function useUserStorage(key: string) {
   const queryClient = useQueryClient();
-  const { data: account } = useAccountQuery();
-  const userEmail = account?.info?.email;
+  const userEmail: string | null = (useJwtEmail() as string) ?? null;
   const [owner, setOwner] = useState<null | string>(null);
-  const isLoggedIn = useIsLoggedIn();
 
   const value = useQuery({
     queryKey: ['user-storage', userEmail, key],
     queryFn: async () => {
-      if (!userEmail || !isLoggedIn) {
-        return;
+      try {
+        const resp = await ofetch<{ value: string }>(
+          `${ACCOUNT_PANEL_ORIGIN}/api/v1/account/user-storage/${key}`,
+        );
+        setOwner(userEmail);
+        return typeof resp.value === 'string' ? resp.value : null;
+      } catch {
+        setOwner(null);
+        return undefinedSymbol;
       }
-      const resp = await ofetch<{ value: string }>(
-        `${ACCOUNT_PANEL_ORIGIN}/api/v1/account/user-storage/${key}`,
-      );
-      setOwner(userEmail);
-      return typeof resp.value === 'string' ? resp.value : null;
     },
-    onError: () => setOwner(null),
-    initialData: undefined,
+    initialData: undefinedSymbol,
   });
 
   const save = useMutation({
     mutationFn: async (newValue: string) => {
-      if (!isLoggedIn || !userEmail) {
-        return null;
-      }
-      try {
-        const resp = await ofetch<{ message?: 'ok' }>(
-          `${ACCOUNT_PANEL_ORIGIN}/api/v1/account/user-storage/${key}`,
-          {
-            body: {
-              value: newValue,
-            },
-            method: 'post',
+      const resp = await ofetch<{ message?: 'ok' }>(
+        `${ACCOUNT_PANEL_ORIGIN}/api/v1/account/user-storage/${key}`,
+        {
+          body: {
+            value: newValue,
           },
-        );
-        return resp.message === 'ok' ? newValue : value.data;
-      } catch {
-        return value.data;
-      }
+          method: 'post',
+        },
+      );
+      return resp.message === 'ok' ? newValue : null;
     },
     onSuccess: newValue =>
       queryClient.setQueryData(['user-storage', userEmail, key], newValue),
@@ -54,31 +47,22 @@ export function useUserStorage(key: string) {
 
   const remove = useMutation({
     mutationFn: async () => {
-      if (!userEmail || !isLoggedIn) {
-        return null;
-      }
-      try {
-        const resp = await ofetch<{ message?: 'ok' }>(
-          `${ACCOUNT_PANEL_ORIGIN}/api/v1/account/user-storage/${key}`,
-          {
-            method: 'delete',
-          },
-        );
-        return resp.message === 'ok';
-      } catch {
-        return false;
-      }
+      await ofetch<{ message?: 'ok' }>(
+        `${ACCOUNT_PANEL_ORIGIN}/api/v1/account/user-storage/${key}`,
+        {
+          method: 'delete',
+        },
+      );
+      return null;
     },
-    onSuccess: newValue => {
-      if (newValue) {
-        queryClient.setQueryData(['user-storage', userEmail, key], null);
-      }
+    onSuccess: () => {
+      queryClient.setQueryData(['user-storage', userEmail, key], null);
     },
   });
 
   useEffect(() => {
     setOwner(null);
-  }, [isLoggedIn]);
+  }, [userEmail]);
 
   const isTrusted =
     !value.isLoading &&
@@ -88,12 +72,11 @@ export function useUserStorage(key: string) {
     value.isFetched &&
     !!userEmail &&
     userEmail === owner &&
-    value.data !== undefined &&
-    isLoggedIn;
+    value.data !== undefinedSymbol;
 
   return {
     isLoading: value.isLoading || save.isLoading || remove.isLoading,
-    value: isTrusted ? value.data ?? null : undefined,
+    value: isTrusted ? (value.data as string) ?? null : undefined,
     save: save.mutateAsync,
     remove: remove.mutateAsync,
   };
