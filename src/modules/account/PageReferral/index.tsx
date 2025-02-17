23beb0d1d68d9eac0bea4406a5aeeed8e96fd9ba
Majid Pouramini <pouramini.majid@gmail.com>
2025-02-17 14:56:05 +0330

    fix decimal for revenue and add bonus bag

--- src/modules/account/PageReferral/index.tsx
+++ src/modules/account/PageReferral/index.tsx
@@ -3,7 +3,6 @@ import { bxCopy, bxShareAlt } from 'boxicons-quasar';
 import { useFriendsQuery, useReferralStatusQuery } from 'api';
 import PageWrapper from 'modules/base/PageWrapper';
 import Badge from 'shared/Badge';
-import Button from 'shared/Button';
 import Icon from 'shared/Icon';
 import { Input } from 'shared/v1-components/Input';
 import { useShare } from 'shared/useShare';
@@ -11,10 +10,15 @@ import { AUTO_TRADER_MINI_APP_BASE } from 'config/constants';
 import { useTelegram } from 'modules/base/mini-app/TelegramProvider';
 import { isMiniApp } from 'utils/version';
 import HowReferralWorks from 'modules/account/PageReferral/HowReferralWorks';
+import { Button } from 'shared/v1-components/Button';
 import trader from './images/trader.png';
 import { ReactComponent as Logo } from './images/logo.svg';
 import { ReactComponent as Users } from './images/users.svg';
 import { ReactComponent as IconUser } from './images/user.svg';
+import { ReactComponent as Bag } from './images/bag.svg';
+import { ReactComponent as Gift } from './images/gift.svg';
+// eslint-disable-next-line import/max-dependencies
+import coin from './images/coin.png';
 
 export default function ReferralPage() {
   const { t } = useTranslation('auth');
@@ -42,7 +46,7 @@ export default function ReferralPage() {
   };
 
   return (
-    <PageWrapper loading={isLoading}>
+    <PageWrapper loading={isLoading} className="mb-24">
       <h1 className="mb-2">{t('page-referral.title')}</h1>
       <p className="mb-2 text-sm text-v1-content-secondary">
         {t('page-referral.subtitle')}
@@ -50,7 +54,7 @@ export default function ReferralPage() {
       <HowReferralWorks />
 
       <div
-        className="inset-x-0 bottom-0 mb-5 rounded-xl bg-v1-surface-l2 p-6 mobile:fixed mobile:mb-0 mobile:rounded-none"
+        className="inset-x-0 bottom-0 z-10 mb-5 rounded-xl bg-v1-surface-l2 p-6 mobile:fixed mobile:mb-0 mobile:rounded-none"
         style={{
           boxShadow:
             '0px -90px 25px 0px rgba(0, 0, 0, 0.00), 0px -57px 23px 0px rgba(0, 0, 0, 0.02), 0px -32px 19px 0px rgba(0, 0, 0, 0.06), 0px -14px 14px 0px rgba(0, 0, 0, 0.11), 0px -4px 8px 0px rgba(0, 0, 0, 0.13)',
@@ -72,7 +76,7 @@ export default function ReferralPage() {
             }
             value={myReferralLink}
           />
-          <Button size="small" variant="primary" onClick={shareLink}>
+          <Button variant="white" onClick={shareLink}>
             <Icon name={bxShareAlt} className="mr-2" />
             {t('page-referral.share')}
           </Button>
@@ -112,7 +116,7 @@ export default function ReferralPage() {
             <Logo />
           </div>
           <div>
-            <h2>${referral?.referral_subscription_revenue}</h2>
+            <h2>${referral?.referral_subscription_revenue.toFixed(2)}</h2>
             <p className="text-xs text-v1-content-secondary">
               {t('page-referral.earned-subscription')}
             </p>
@@ -124,7 +128,7 @@ export default function ReferralPage() {
             <img src={trader} className="h-6 w-6 rounded-full" alt="" />
           </div>
           <div>
-            <h2>${referral?.referral_trade_revenue}</h2>
+            <h2>${referral?.referral_trade_revenue.toFixed(2)}</h2>
             <p className="text-xs text-v1-content-secondary">
               {t('page-referral.earned-trader')}
             </p>
@@ -132,28 +136,37 @@ export default function ReferralPage() {
         </div>
       </div>
 
-      {/* <h2 className="mb-2 mt-5">{t('page-referral.bonus.title')}</h2> */}
-      {/* <div */}
-      {/*   className="flex justify-between overflow-hidden rounded-xl" */}
-      {/*   style={{ */}
-      {/*     background: 'linear-gradient(126deg, #625134 -2.76%, #F7D57E 100%)', */}
-      {/*   }} */}
-      {/* > */}
-      {/*   <div className="grow p-3"> */}
-      {/*     <div className="flex items-center gap-3"> */}
-      {/*       <div className="flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-v1-inverse-overlay-100/5"> */}
-      {/*         <Bag /> */}
-      {/*       </div> */}
-      {/*       <div> */}
-      {/*         <h3>${referral?.referral_revenue}</h3> */}
-      {/*         <p className="text-xs">{t('page-referral.bonus.ready')}</p> */}
-      {/*       </div> */}
-      {/*     </div> */}
-      {/*     <hr className="my-3 border-v1-border-primary/30" /> */}
-      {/*     <p className="text-xs">{t('page-referral.bonus.description')}</p> */}
-      {/*   </div> */}
-      {/*   <img src={coin} className="h-40" alt="" /> */}
-      {/* </div> */}
+      <h2 className="mb-2 mt-5">{t('page-referral.bonus.title')}</h2>
+      <div
+        className="flex justify-between overflow-hidden rounded-xl"
+        style={{
+          background: 'linear-gradient(126deg, #625134 -2.76%, #F7D57E 100%)',
+        }}
+      >
+        <div className="grow p-3">
+          <div className="flex items-center gap-3">
+            <div className="flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-v1-inverse-overlay-100/5">
+              <Bag />
+            </div>
+            <div>
+              <h3>${referral?.ready_to_claim.toFixed(2)}</h3>
+              <p className="text-xs">{t('page-referral.bonus.ready')}</p>
+            </div>
+          </div>
+          <hr className="my-3 border-v1-border-primary/30" />
+          <p className="text-xs">{t('page-referral.bonus.description')}</p>
+          <Button
+            variant="white"
+            className="mt-3 w-full "
+            size="md"
+            disabled={true}
+          >
+            <Gift />
+            {t('page-referral.bonus.claim')}
+          </Button>
+        </div>
+        <img src={coin} className="h-40" alt="" />
+      </div>
 
       <div>
         <div className="mb-2 mt-5 flex items-center gap-3">
