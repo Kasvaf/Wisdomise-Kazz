4bf9f2ad4bf368811fde96d77645f3fb3392c972
arash16 <arash.shakery@gmail.com>
2025-02-18 13:55:57 +0330

    fix sol provider

--- src/modules/base/wallet/WalletProvider.tsx
+++ src/modules/base/wallet/WalletProvider.tsx
@@ -1,14 +1,17 @@
 import { useMemo, type PropsWithChildren } from 'react';
 import { THEME, TonConnectUIProvider } from '@tonconnect/ui-react';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import { UnifiedWalletProvider } from '@jup-ag/wallet-adapter';
+import {
+  ConnectionProvider,
+  UnifiedWalletProvider,
+} from '@jup-ag/wallet-adapter';
 import { WalletConnectWalletAdapter } from '@solana/wallet-adapter-walletconnect';
 import { AUTO_TRADER_MINI_APP_BASE } from 'config/constants';
 import { LayoutActiveNetworkProvider } from '../active-network';
 import WalletEvents from './WalletEvents';
 
 // https://www.comparenodes.com/library/public-endpoints/solana/
-// const rpcEndpoint = 'https://solana-rpc.publicnode.com';
+const rpcEndpoint = 'https://solana-rpc.publicnode.com';
 
 const SolanaWalletProvider: React.FC<PropsWithChildren> = ({ children }) => {
   const network = WalletAdapterNetwork.Mainnet;
@@ -33,27 +36,29 @@ const SolanaWalletProvider: React.FC<PropsWithChildren> = ({ children }) => {
   );
 
   return (
-    <UnifiedWalletProvider
-      wallets={wallets}
-      config={{
-        autoConnect: false,
-        theme: 'dark',
-        env: 'mainnet-beta',
-        metadata: {
-          name: 'Wisdomise AutoTrader',
-          description:
-            'Track whales, spot moonshots, and auto-trade like a true degen — all in a few clicks. 🚀👀',
-          url: 'https://app.wisdomise.com',
-          iconUrls: ['http://wisdomise.com/icon.svg'],
-        },
-        // notificationCallback: WalletNotification,
-        // walletlistExplanation: {
-        //   href: 'https://station.jup.ag/docs/additional-topics/wallet-list',
-        // },
-      }}
-    >
-      {children}
-    </UnifiedWalletProvider>
+    <ConnectionProvider endpoint={rpcEndpoint}>
+      <UnifiedWalletProvider
+        wallets={wallets}
+        config={{
+          autoConnect: false,
+          theme: 'dark',
+          env: 'mainnet-beta',
+          metadata: {
+            name: 'Wisdomise AutoTrader',
+            description:
+              'Track whales, spot moonshots, and auto-trade like a true degen — all in a few clicks. 🚀👀',
+            url: 'https://app.wisdomise.com',
+            iconUrls: ['http://wisdomise.com/icon.svg'],
+          },
+          // notificationCallback: WalletNotification,
+          // walletlistExplanation: {
+          //   href: 'https://station.jup.ag/docs/additional-topics/wallet-list',
+          // },
+        }}
+      >
+        {children}
+      </UnifiedWalletProvider>
+    </ConnectionProvider>
   );
 };
 
