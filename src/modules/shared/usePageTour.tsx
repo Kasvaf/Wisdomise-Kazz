4c07eb7417f8473fbe2a5c11eddf95c9f8567a0a
arash16 <arash.shakery@gmail.com>
2025-03-09 10:57:00 +0330

    refactor usePageTour

---
+++ src/modules/shared/usePageTour.tsx
@@ -0,0 +1,27 @@
+import { useTour, type StepType } from '@reactour/tour';
+import { useLocalStorage } from '@solana/wallet-adapter-react';
+import { useEffect } from 'react';
+
+const usePageTour = ({
+  enabled,
+  steps,
+  key,
+}: {
+  enabled?: boolean;
+  steps: StepType[];
+  key: string;
+}) => {
+  const [seen, setSeen] = useLocalStorage('seen-' + key, false);
+  const { setCurrentStep, setSteps, setIsOpen } = useTour();
+
+  useEffect(() => {
+    if (!enabled || seen) return;
+    setSeen(true);
+    setSteps?.(steps);
+    setCurrentStep(0);
+    setIsOpen(true);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [enabled, seen, steps]);
+};
+
+export default usePageTour;
