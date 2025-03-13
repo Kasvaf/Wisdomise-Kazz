9d55d2ed3e36d7d4687c396debef5f434eae8b8e
Majid Pouramini <pouramini.majid@gmail.com>
2025-03-13 13:56:35 +0330

    remove splash on embedded view

--- src/modules/base/Splash.tsx
+++ src/modules/base/Splash.tsx
@@ -1,8 +1,12 @@
 import { useTranslation } from 'react-i18next';
 import { ReactComponent as Logo } from 'assets/logo-vertical.svg';
+import { useEmbedView } from 'modules/embedded/useEmbedView';
 
 const Splash = () => {
   const { t } = useTranslation();
+  const { isEmbeddedView } = useEmbedView();
+  if (isEmbeddedView) return null;
+
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center">
       <Logo className="mb-[50px] h-[200px] w-full" />
