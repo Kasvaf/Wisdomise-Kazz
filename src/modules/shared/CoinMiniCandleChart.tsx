064552b3cc99dc1b48d0329b29b25c84f4b474f0
Amir Momenian <nainemom@gmail.com>
2025-03-19 00:04:07 +0330

    create CoinMiniCandleChart component

---
+++ src/modules/shared/CoinMiniCandleChart.tsx
@@ -0,0 +1,56 @@
+import { type ComponentProps, type FC } from 'react';
+import { type EChartsOption } from 'echarts';
+import { useMemo } from 'react';
+import { ECharts } from 'shared/ECharts';
+import { type Candle } from 'api';
+import { useChangesDebugger } from 'utils/debug';
+
+export const CoinMiniCandleChart: FC<
+  Omit<ComponentProps<typeof ECharts>, 'options'> & {
+    value: Candle[];
+  }
+> = ({ value, ...props }) => {
+  const chartConfig = useMemo<EChartsOption>(() => {
+    return {
+      grid: {
+        top: 5,
+        bottom: 5,
+        left: 5,
+        right: 5,
+        containLabel: false,
+      },
+      xAxis: {
+        type: 'category',
+        data: value.map(x => x.related_at),
+        boundaryGap: false,
+        show: false,
+      },
+      yAxis: {
+        scale: true,
+        show: false,
+        boundaryGap: false,
+      },
+      tooltip: {
+        show: false,
+      },
+      series: [
+        {
+          type: 'candlestick',
+          data: value.map(row => [row.open, row.close, row.low, row.high]),
+        },
+      ],
+      backgroundColor: 'transparent',
+    };
+  }, [value]);
+
+  useChangesDebugger([value.length], ['value']);
+
+  return (
+    <ECharts
+      key={JSON.stringify(value)}
+      height={125}
+      {...props}
+      options={chartConfig}
+    />
+  );
+};
