862a39ed375920c84a9e06318c2cb84017eaa6cc
Amir Momenian <nainemom@gmail.com>
2025-04-06 18:24:00 +0330

    fix splash state

--- src/modules/base/auth/AuthGuard.tsx
+++ src/modules/base/auth/AuthGuard.tsx
@@ -6,9 +6,9 @@ import Splash from 'modules/base/Splash';
 const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID as string;
 
 export default function AuthGuard({ children }: PropsWithChildren) {
-  const { isPending, isFetching } = useAccountQuery();
+  const { isPending, isLoading } = useAccountQuery();
 
-  return isPending || isFetching ? (
+  return isPending || isLoading ? (
     <Splash />
   ) : (
     <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
