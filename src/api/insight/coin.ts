242dbdc8a17453b0b17c2215d3138124754dc85b
Amir Momenian <nainemom@gmail.com>
2025-04-06 19:54:04 +0330

    update coin radar coins staleTime

--- src/api/insight/coin.ts
+++ src/api/insight/coin.ts
@@ -60,7 +60,7 @@ export const useCoinRadarCoins = (config: { networks?: string[] }) =>
 
         return true;
       }),
-    refetchInterval: 1000 * 60 * 5,
+    refetchInterval: 1000 * 30,
     gcTime: Number.POSITIVE_INFINITY,
-    staleTime: 1000 * 60 * 5,
+    staleTime: 1000 * 30,
   });
