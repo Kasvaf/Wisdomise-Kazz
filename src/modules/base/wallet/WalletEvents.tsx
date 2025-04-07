343ed2b34f4116e00720f77c8c7fd051eba93a06
Majid Pouramini <pouramini.majid@gmail.com>
2025-04-07 09:48:06 +0330

    tournament desktop

--- src/modules/base/wallet/WalletEvents.tsx
+++ src/modules/base/wallet/WalletEvents.tsx
@@ -1,11 +1,11 @@
 import { useEffect, type PropsWithChildren } from 'react';
-import { useTonAddress } from '@tonconnect/ui-react';
 import { useLocalStorage } from 'usehooks-ts';
 import { gtag } from 'config/gtag';
+import { useActiveWallet } from 'api/chains';
 
 const WalletEvents: React.FC<PropsWithChildren> = ({ children }) => {
   // TODO: send both solana and ton wallet connect events to segment
-  const address = useTonAddress();
+  const { address } = useActiveWallet();
   const [walletConnectedFirstTime, setWalletConnected] = useLocalStorage(
     'wallet-connected-first-time',
     false,
