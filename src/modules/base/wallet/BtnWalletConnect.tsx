022aa23b55d10133f40e4ecbb72dec21d6fb825a
arash16 <arash.shakery@gmail.com>
2025-04-10 13:22:47 +0330

    fix wallet-connect button's height in desktop

--- src/modules/base/wallet/BtnWalletConnect.tsx
+++ src/modules/base/wallet/BtnWalletConnect.tsx
@@ -1,19 +1,22 @@
+import { clsx } from 'clsx';
 import { TonConnectButton } from '@tonconnect/ui-react';
 import { useWallet } from '@solana/wallet-adapter-react';
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 import { useActiveNetwork } from 'modules/base/active-network';
 import '@solana/wallet-adapter-react-ui/styles.css';
+import useIsMobile from 'utils/useIsMobile';
 
 const BtnSolanaWalletConnect: React.FC<{ className?: string }> = ({
   className,
 }) => {
+  const isMobile = useIsMobile();
   const solanaWallet = useWallet();
   const addr = solanaWallet.publicKey?.toString() || '';
   return (
     <WalletMultiButton
       style={{
         borderRadius: 20,
-        height: 40,
+        height: isMobile ? 40 : 32,
         fontSize: 14,
         lineHeight: 'normal',
         ...(solanaWallet.connected
@@ -30,10 +33,11 @@ const BtnSolanaWalletConnect: React.FC<{ className?: string }> = ({
 };
 
 const BtnWalletConnect: React.FC<{ className?: string }> = ({ className }) => {
+  const isMobile = useIsMobile();
   const net = useActiveNetwork();
 
   return net === 'the-open-network' ? (
-    <TonConnectButton className={className} />
+    <TonConnectButton className={clsx(className, !isMobile && 'h-[32px]')} />
   ) : net === 'solana' ? (
     <BtnSolanaWalletConnect className={className} />
   ) : null;
