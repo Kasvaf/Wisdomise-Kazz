0ec4293304e3e25292ced70da0d533e8ed1094ac
arash16 <arash.shakery@gmail.com>
2025-04-17 01:27:52 +0330

    no inline-trader if not supported

--- src/modules/insight/PageCoinDetails/components/CoinDetailsDesktop/index.tsx
+++ src/modules/insight/PageCoinDetails/components/CoinDetailsDesktop/index.tsx
@@ -4,6 +4,7 @@ import UserAssets from 'modules/autoTrader/UserAssets';
 import Trader from 'modules/autoTrader/PageTrade/Trader';
 import useSearchParamAsState from 'shared/useSearchParamAsState';
 import { ActiveNetworkProvider } from 'modules/base/active-network';
+import { useSupportedPairs } from 'api';
 import { type AutoTraderSupportedQuotes } from 'api/chains';
 import { CoinStatsWidget } from '../CoinStatsWidget';
 import { CoinDetailsTabs } from '../CoinDetailsTabs';
@@ -23,6 +24,7 @@ import { CoinIntroductionWidget } from '../CoinIntroductionWidget';
 export const CoinDetailsDesktop: FC<{ slug: string }> = ({ slug }) => {
   const root = useRef<HTMLDivElement>(null);
   const tabs = useCoinDetailsTabs(root);
+  const { data: supportedPairs } = useSupportedPairs(slug);
   const [quote, setQuote] = useSearchParamAsState<AutoTraderSupportedQuotes>(
     'quote',
     'tether',
@@ -32,21 +34,19 @@ export const CoinDetailsDesktop: FC<{ slug: string }> = ({ slug }) => {
     <div className="flex flex-nowrap justify-between">
       {/* Discover */}
       <div className="relative w-1/4 min-w-60 max-w-80 p-3 ps-0">
-        <div className="space-y-3">
-          <div className="[&:not(:has(.id-assets))]:hidden">
-            <h3 className="mb-2 text-xxs">Your Portfolio</h3>
-            <div className="space-y-4 rounded-md bg-v1-surface-l2 p-3">
-              <UserAssets
-                noTotal
-                className="id-assets"
-                containerClassName="!p-0"
-              />
-            </div>
-            <hr className="mt-3 border-white/10" />
+        <div className="[&:not(:has(.id-assets))]:hidden">
+          <h3 className="mb-2 text-xxs">Your Portfolio</h3>
+          <div className="space-y-4 rounded-md bg-v1-surface-l2 p-3">
+            <UserAssets
+              noTotal
+              className="id-assets"
+              containerClassName="!p-0"
+            />
           </div>
-
-          <CoinDiscoverWidget />
+          <hr className="my-3 border-white/10" />
         </div>
+
+        <CoinDiscoverWidget />
       </div>
 
       {/* Validate */}
@@ -88,15 +88,20 @@ export const CoinDetailsDesktop: FC<{ slug: string }> = ({ slug }) => {
       </div>
 
       {/* Trade + Additional */}
-      <div className="relative w-1/3 min-w-72 max-w-96 p-3 pe-0">
-        <div className="sticky top-[100px] space-y-3">
-          <h3 className="mb-2 text-xxs">Auto Trade</h3>
-          <div className="space-y-4 rounded-md bg-v1-surface-l2 p-3 [&_.id-line]:hidden">
-            <ActiveNetworkProvider base={slug} quote={quote} setOnLayout>
-              <Trader quote={quote} setQuote={setQuote} slug={slug} />
-            </ActiveNetworkProvider>
-          </div>
-          <hr className="border-white/10" />
+      <div className="relative w-1/3 min-w-72 max-w-96 p-3 pe-0 pt-0">
+        <div className="sticky top-[88px]">
+          {Boolean(supportedPairs?.length) && (
+            <>
+              <h3 className="mb-2 text-xxs">Auto Trade</h3>
+              <div className="space-y-4 rounded-md bg-v1-surface-l2 p-3 [&_.id-line]:hidden">
+                <ActiveNetworkProvider base={slug} quote={quote} setOnLayout>
+                  <Trader quote={quote} setQuote={setQuote} slug={slug} />
+                </ActiveNetworkProvider>
+              </div>
+              <hr className="border-white/10" />
+            </>
+          )}
+
           <CoinStatsWidget slug={slug} />
           <NCoinStatsWidget slug={slug} />
         </div>
