05a3683e8d2738b9a9741f23ccfab0f3327e80ad
arash16 <arash.shakery@gmail.com>
2025-04-23 12:22:49 +0400

    placeholder for coins without trading support

--- src/modules/insight/PageCoinDetails/components/CoinDetailsDesktop/index.tsx
+++ src/modules/insight/PageCoinDetails/components/CoinDetailsDesktop/index.tsx
@@ -1,11 +1,6 @@
 /* eslint-disable import/max-dependencies */
 import { type FC, useRef } from 'react';
 import UserAssets from 'modules/autoTrader/UserAssets';
-import Trader from 'modules/autoTrader/PageTrade/Trader';
-import useSearchParamAsState from 'shared/useSearchParamAsState';
-import { ActiveNetworkProvider } from 'modules/base/active-network';
-import { useSupportedPairs } from 'api';
-import { type AutoTraderSupportedQuotes } from 'api/chains';
 import { CoinStatsWidget } from '../CoinStatsWidget';
 import { CoinDetailsTabs } from '../CoinDetailsTabs';
 import { useCoinDetailsTabs } from '../../hooks/useCoinDetailsTabs';
@@ -21,15 +16,11 @@ import { CoinPoolsWidget } from '../CoinPoolsWidget';
 import { CoinWhalesWidget } from '../CoinWhalesWidget';
 import { CoinIntroductionWidget } from '../CoinIntroductionWidget';
 import { NCoinRisksBanner } from '../NCoinRisksBanner';
+import TraderSection from './TraderSection';
 
 export const CoinDetailsDesktop: FC<{ slug: string }> = ({ slug }) => {
   const root = useRef<HTMLDivElement>(null);
   const tabs = useCoinDetailsTabs(root);
-  const { data: supportedPairs } = useSupportedPairs(slug);
-  const [quote, setQuote] = useSearchParamAsState<AutoTraderSupportedQuotes>(
-    'quote',
-    'tether',
-  );
 
   return (
     <div className="flex flex-nowrap justify-between">
@@ -92,17 +83,8 @@ export const CoinDetailsDesktop: FC<{ slug: string }> = ({ slug }) => {
       {/* Trade + Additional */}
       <div className="relative w-1/3 min-w-72 max-w-96 p-3 pe-0 pt-0">
         <div className="sticky top-[88px] space-y-4">
-          {Boolean(supportedPairs?.length) && (
-            <>
-              <h3 className="mb-2 text-xxs">Auto Trade</h3>
-              <div className="space-y-4 rounded-md bg-v1-surface-l2 p-3 [&_.id-line]:hidden">
-                <ActiveNetworkProvider base={slug} quote={quote} setOnLayout>
-                  <Trader quote={quote} setQuote={setQuote} slug={slug} />
-                </ActiveNetworkProvider>
-              </div>
-              <hr className="border-white/10" />
-            </>
-          )}
+          <TraderSection slug={slug} />
+          <hr className="border-white/10" />
 
           <CoinStatsWidget slug={slug} />
           <NCoinStatsWidget slug={slug} />
