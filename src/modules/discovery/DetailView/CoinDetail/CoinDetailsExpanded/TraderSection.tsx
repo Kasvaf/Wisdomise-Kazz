021421934a8b281176242d99c7dd5da1a57abbc4
arash16 <arash.shakery@gmail.com>
2025-05-21 14:43:27 +0330

    error when pairs is not loaded

--- src/modules/discovery/DetailView/CoinDetail/CoinDetailsExpanded/TraderSection.tsx
+++ src/modules/discovery/DetailView/CoinDetail/CoinDetailsExpanded/TraderSection.tsx
@@ -7,7 +7,7 @@ import BuySellTrader from 'modules/autoTrader/BuySellTrader';
 import { ReactComponent as TradingIcon } from './TradingIcon.svg';
 
 const TraderSection: React.FC<{ slug: string }> = ({ slug }) => {
-  const { data: supportedPairs, isLoading } = useSupportedPairs(slug);
+  const { data: supportedPairs, isLoading, error } = useSupportedPairs(slug);
   const [quote, setQuote] = useSearchParamAsState<string>('quote', 'tether');
 
   return (
@@ -26,6 +26,11 @@ const TraderSection: React.FC<{ slug: string }> = ({ slug }) => {
               loadingClassName="bg-v1-surface-l1"
             />
           </ActiveNetworkProvider>
+        ) : error ? (
+          <div className="my-8 flex flex-col items-center gap-3 text-center text-sm">
+            <TradingIcon className="size-8" />
+            We had an issue contacting our servers. Please try again later.
+          </div>
         ) : (
           <div className="my-8 flex flex-col items-center gap-3 text-center text-sm">
             <TradingIcon className="size-8" />
