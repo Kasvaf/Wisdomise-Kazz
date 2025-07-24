b82e51cdb2cfa427ea3314f5addc6c757dbbf3f8
Amir Momenian <nainemom@gmail.com>
2025-07-24 13:45:33 +0330

    temp: just for roohi

--- src/modules/discovery/DetailView/CoinDetail/CoinTitleWidget.tsx
+++ src/modules/discovery/DetailView/CoinDetail/CoinTitleWidget.tsx
@@ -4,15 +4,12 @@ import { clsx } from 'clsx';
 import { useTranslation } from 'react-i18next';
 import { NCoinAge } from 'modules/discovery/ListView/NetworkRadar/NCoinAge';
 import { NCoinBuySell } from 'modules/discovery/ListView/NetworkRadar/NCoinBuySell';
-import { useCoinDetails, useNCoinDetails } from 'api/discovery';
 import { ReadableNumber } from 'shared/ReadableNumber';
 import { useLoadingBadge } from 'shared/LoadingBadge';
 import { NCoinDeveloper } from 'modules/discovery/ListView/NetworkRadar/NCoinDeveloper';
-import {
-  calcNCoinRiskLevel,
-  doesNCoinHaveLargeTxns,
-} from 'modules/discovery/ListView/NetworkRadar/lib';
+import { doesNCoinHaveLargeTxns } from 'modules/discovery/ListView/NetworkRadar/lib';
 import { Coin } from 'shared/v1-components/Coin';
+import { useUnifiedCoinDetails } from './useUnifiedCoinDetails';
 
 export const CoinTitleWidget: FC<{
   slug: string;
@@ -21,15 +18,7 @@ export const CoinTitleWidget: FC<{
   suffix?: ReactNode;
 }> = ({ slug, className, hr, suffix }) => {
   const { t } = useTranslation('network-radar');
-  const coin = useCoinDetails({ slug });
-  const nCoin = useNCoinDetails({ slug });
-  const isLoading =
-    coin.isLoading || nCoin.isLoading || coin.isPending || nCoin.isPending;
-  const isNCoin = !!nCoin.data?.base_symbol;
-  const symbol = nCoin.data?.base_symbol || coin.data?.symbol;
-  const nCoinRiskLevel = calcNCoinRiskLevel({
-    riskPercent: nCoin.data?.risk_percent ?? 0,
-  });
+  const { data, isLoading } = useUnifiedCoinDetails({ slug });
 
   useLoadingBadge(isLoading);
 
@@ -38,43 +27,36 @@ export const CoinTitleWidget: FC<{
       <div
         className={clsx(
           'flex items-center gap-1 overflow-auto whitespace-nowrap mobile:flex-col',
-          symbol ? 'justify-between' : 'justify-center',
+          data.symbol ? 'justify-between' : 'justify-center',
           className,
         )}
       >
-        {symbol ? (
+        {data.symbol ? (
           <>
             <div className="flex w-full items-center justify-start gap-2 mobile:w-full mobile:flex-wrap">
               <Coin
-                abbreviation={symbol.abbreviation}
-                name={symbol.name}
-                slug={symbol.slug}
-                logo={symbol.logo_url}
+                abbreviation={data.symbol.abbreviation}
+                name={data.symbol.name}
+                slug={data.symbol.slug}
+                logo={data.symbol.logo_url}
                 href={false}
-                categories={coin.data?.symbol.categories}
-                networks={isNCoin ? [] : coin.data?.networks}
-                labels={coin.data?.symbol_labels?.filter(x => !!x)}
-                security={
-                  isNCoin
-                    ? []
-                    : coin.data?.security_data?.map(x => x.symbol_security)
-                }
-                links={
-                  nCoin.data?.base_community_data.links ||
-                  coin.data?.community_data?.links
-                }
+                categories={data.symbol.categories}
+                networks={data.networks}
+                labels={data.labels}
+                security={data.goPlusSecurity}
+                links={data.links}
                 customLabels={
                   <>
                     {/* Developer Data */}
-                    {nCoin.data?.dev && (
-                      <NCoinDeveloper value={nCoin.data.dev} />
+                    {data?.developer && (
+                      <NCoinDeveloper value={data.developer} />
                     )}
 
-                    {nCoin.data?.creation_datetime && (
+                    {data.createdAt && (
                       <>
                         <span className="size-[2px] rounded-full bg-white" />
                         <NCoinAge
-                          value={nCoin.data?.creation_datetime}
+                          value={data.createdAt}
                           inline
                           className="text-xs"
                         />
@@ -85,7 +67,7 @@ export const CoinTitleWidget: FC<{
                 block
                 truncate={false}
               />
-              {nCoin.data && (
+              {data.tradesData && (
                 <div className="flex items-center justify-start gap-4 mobile:w-full mobile:justify-between mobile:gap-2">
                   <div className="h-4 w-px bg-white/10 mobile:hidden" />
                   <div className="flex flex-col justify-between">
@@ -93,16 +75,16 @@ export const CoinTitleWidget: FC<{
                       {t('common.buy_sell')}
                       {' (24h)'}
                       {doesNCoinHaveLargeTxns({
-                        totalNumBuys: nCoin.data.update.total_num_buys ?? 0,
-                        totalNumSells: nCoin.data.update.total_num_sells ?? 0,
+                        totalNumBuys: data.tradesData.total_num_buys ?? 0,
+                        totalNumSells: data.tradesData.total_num_sells ?? 0,
                       })
                         ? ' 🔥'
                         : ''}
                     </p>
                     <NCoinBuySell
                       value={{
-                        buys: nCoin.data?.update?.total_num_buys,
-                        sells: nCoin.data?.update?.total_num_sells,
+                        buys: data.tradesData.total_num_buys,
+                        sells: data.tradesData.total_num_sells,
                       }}
                       className="text-xs"
                       imgClassName="size-4"
@@ -115,7 +97,7 @@ export const CoinTitleWidget: FC<{
                       {' (24h)'}
                     </p>
                     <ReadableNumber
-                      value={nCoin.data.update.trading_volume.usd}
+                      value={data.marketData.trading_volume_24h}
                       className="text-xs"
                       label="$"
                       popup="never"
@@ -130,20 +112,20 @@ export const CoinTitleWidget: FC<{
                     <span className="text-xs">
                       <span
                         className={clsx(
-                          nCoinRiskLevel === 'low'
+                          data.risks?.level === 'low'
                             ? 'text-v1-content-positive'
-                            : nCoinRiskLevel === 'medium'
+                            : data.risks?.level === 'medium'
                             ? 'text-v1-content-notice'
                             : 'text-v1-content-negative',
                         )}
                       >
-                        {nCoin.data.risk_percent ?? 0}
+                        {data.risks?.percentage ?? 0}
                       </span>
                       <span className="text-v1-content-secondary">/100</span>
                     </span>
                   </div>
 
-                  {nCoin.data.rugged && (
+                  {data.rugCheckSecurity.rugged && (
                     <>
                       <div className="h-4 w-px bg-white/10" />
                       <span className="rounded-full bg-v1-content-negative/10 p-1 px-2 text-xs text-v1-content-negative">
@@ -166,7 +148,7 @@ export const CoinTitleWidget: FC<{
           </p>
         )}
       </div>
-      {hr && symbol && <hr className="border-white/10" />}
+      {hr && data.symbol && !isLoading && <hr className="border-white/10" />}
     </>
   );
 };
---
+++ src/modules/discovery/DetailView/CoinDetail/useUnifiedCoinDetails.ts
@@ -0,0 +1,272 @@
+import { useMemo } from 'react';
+import {
+  type CoinCommunityData,
+  type CoinNetwork,
+  type NetworkRadarNCoinDetails,
+  useCoinDetails,
+  useNCoinDetails,
+} from 'api/discovery';
+import { networkRadarGrpc } from 'api/grpc';
+import { type Coin } from 'api/types/shared';
+import {
+  calcNCoinRiskLevel,
+  convertNCoinSecurityFieldToBool,
+  doesNCoinHaveSafeTopHolders,
+} from 'modules/discovery/ListView/NetworkRadar/lib';
+
+export const useUnifiedCoinDetails = ({ slug }: { slug: string }) => {
+  const [network, base] = slug.startsWith('solana_')
+    ? slug.split('_')
+    : ['', ''];
+  const resp1 = useCoinDetails({ slug });
+  const resp2 = useNCoinDetails({ slug });
+  const resp3 = networkRadarGrpc.useCoinDetailStreamLastValue({
+    network,
+    base,
+  });
+
+  const data1 = resp1.data;
+  const data2 = resp2.data;
+  const data3 = network.length > 0 && base.length > 0 ? resp3.data : null;
+
+  console.log(data3, {
+    network,
+    base,
+  });
+
+  const symbol = useMemo<Coin>(() => {
+    return {
+      slug,
+      abbreviation:
+        data3?.symbol?.abbreviation ??
+        data2?.base_symbol?.abbreviation ??
+        data1?.symbol?.abbreviation ??
+        slug,
+      name:
+        data3?.symbol?.name ??
+        data2?.base_symbol?.name ??
+        data1?.symbol?.name ??
+        slug,
+      logo_url:
+        data3?.symbol?.imageUrl ??
+        data2?.base_symbol?.logo_url ??
+        data1?.symbol?.logo_url ??
+        '',
+      categories:
+        data2?.base_symbol?.categories ?? data1?.symbol?.categories ?? [],
+    };
+  }, [data1, data2, data3, slug]);
+
+  const labels = useMemo<string[]>(() => {
+    return data2?.base_symbol_labels ?? data1?.symbol_labels ?? [];
+  }, [data1, data2]);
+
+  const networks = useMemo<CoinNetwork[]>(() => {
+    if (data1?.networks) return data1.networks;
+    if (data2?.network) {
+      return [
+        {
+          network: data2?.network,
+          contract_address: data2.base_contract_address,
+          symbol_network_type: data2.base_contract_address ? 'TOKEN' : 'COIN',
+        },
+      ];
+    }
+    if (data3?.symbol?.contractAddress) {
+      return [
+        {
+          contract_address: data3?.symbol?.contractAddress,
+          network: {
+            name: 'Solana',
+            slug: network,
+            icon_url: '',
+          },
+          symbol_network_type: data3?.symbol?.contractAddress
+            ? 'TOKEN'
+            : 'COIN',
+        },
+      ];
+    }
+    return [];
+  }, [data1, data2, data3, network]);
+
+  const marketData = useMemo(() => {
+    const currentPrice =
+      data2?.update.base_market_data.current_price ??
+      data1?.data?.current_price ??
+      null;
+    const priceChange24h =
+      data2?.update.base_market_data.price_change_24h ??
+      data1?.data?.price_change_24h ??
+      null;
+    const priceChangePercentage24h =
+      currentPrice && priceChange24h
+        ? (currentPrice / (currentPrice + priceChange24h)) * 100 - 100
+        : null;
+    const marketCap =
+      data3?.networkData?.marketCap ??
+      data2?.update.base_market_data.market_cap ??
+      data1?.data?.market_cap;
+    const marketCapChangePercentage24h =
+      data1?.data?.market_cap_change_percentage_24h ?? null;
+
+    const totalVolume =
+      data3?.networkData?.volume ??
+      data2?.update.base_market_data.total_volume ??
+      data1?.data?.total_volume;
+    const totalVolumeChangePercentage24h =
+      data2?.update.base_market_data.volume_change_1d ?? null;
+
+    const tradingVolume24h = data2?.update.trading_volume.usd ?? null;
+
+    const fullyDilutedValuation = data1?.data?.fully_diluted_valuation ?? null;
+
+    const totalSupply =
+      (data3?.networkData?.totalSupply
+        ? +data3?.networkData?.totalSupply
+        : null) ??
+      data2?.update.base_market_data.total_supply ??
+      data1?.data?.total_supply ??
+      null;
+    const maxSupply = data1?.data?.max_supply ?? null;
+    const circulatingSupply = data1?.data?.circulating_supply ?? null;
+
+    const boundingCurve = data3?.networkData?.boundingCurve ?? null;
+
+    return {
+      current_price: currentPrice,
+      price_change_24h: priceChange24h,
+      price_change_percentage_24h: priceChangePercentage24h,
+      market_cap: marketCap,
+      market_cap_change_percentage_24h: marketCapChangePercentage24h,
+      total_volume: totalVolume,
+      total_volume_change_percentage_24h: totalVolumeChangePercentage24h,
+      fully_diluted_valuation: fullyDilutedValuation,
+      total_supply: totalSupply,
+      max_supply: maxSupply,
+      circulating_supply: circulatingSupply,
+      bounding_curve: boundingCurve,
+      trading_volume_24h: tradingVolume24h,
+    };
+  }, [data1, data2, data3]);
+
+  const goPlusSecurity = useMemo(() => {
+    return data1?.security_data
+      ? data1.security_data.map(x => x.symbol_security)
+      : null;
+  }, [data1]);
+
+  const rugCheckSecurity = useMemo(() => {
+    return {
+      freezable:
+        data3?.securityData?.freezable ??
+        convertNCoinSecurityFieldToBool({
+          value: data2?.base_symbol_security.freezable ?? '0',
+          type: 'freezable',
+        }),
+      lpBurned:
+        data3?.securityData?.lpBurned ??
+        convertNCoinSecurityFieldToBool({
+          value: data2?.base_symbol_security.lp_is_burned ?? '0',
+          type: 'lpBurned',
+        }),
+      mintable:
+        data3?.securityData?.mintable ??
+        convertNCoinSecurityFieldToBool({
+          value: data2?.base_symbol_security.mintable ?? '0',
+          type: 'mintable',
+        }),
+      safeTopHolders: doesNCoinHaveSafeTopHolders({
+        topHolders:
+          data3?.validatedData?.top10Holding ??
+          data2?.base_symbol_security.holders.map(x => x.balance) ??
+          0,
+        totalSupply: marketData.total_supply ?? 0,
+      }),
+      rugged: data2?.rugged ?? false,
+    };
+  }, [data2, data3, marketData]);
+
+  const risks = useMemo<{
+    level: 'low' | 'medium' | 'high';
+    list: NonNullable<NetworkRadarNCoinDetails['risks']>;
+    percentage: number;
+  } | null>(() => {
+    return data2?.risks && data2?.risk_percent
+      ? {
+          level: calcNCoinRiskLevel({
+            riskPercent: data2?.risk_percent ?? 0,
+          }),
+          list: data2.risks ?? [],
+          percentage: data2.risk_percent ?? 0,
+        }
+      : null;
+  }, [data2]);
+
+  const validatedData = useMemo(() => data3?.validatedData ?? null, [data3]);
+
+  const links = useMemo<CoinCommunityData['links'] | null>(() => {
+    return (
+      data2?.base_community_data.links ??
+      data1?.community_data?.links ??
+      (data3
+        ? {
+            telegram_channel_identifier: data3.socials?.telegram,
+            twitter_screen_name: data3.socials?.twitter,
+
+            homepage: data3.socials?.website ? [data3.socials?.website] : [],
+          }
+        : null) ??
+      null
+    );
+  }, [data3, data2, data1]);
+
+  const createdAt = useMemo(() => {
+    return data2?.creation_datetime ?? null;
+  }, [data2]);
+
+  const developer = useMemo(() => {
+    return data2?.dev ?? null;
+  }, [data2]);
+
+  const tradesData = useMemo(() => {
+    const totalNumBuys =
+      data3?.networkData?.totalBuy ?? data2?.update.total_num_buys ?? null;
+    const totalNumSells =
+      data3?.networkData?.totalSell ?? data2?.update.total_num_sells ?? null;
+    return typeof totalNumBuys === 'number' && typeof totalNumSells === 'number'
+      ? {
+          total_num_buys: totalNumBuys,
+          total_num_sells: totalNumSells,
+        }
+      : null;
+  }, [data2, data3]);
+
+  return {
+    data: {
+      symbol,
+      labels,
+      networks,
+      marketData,
+      goPlusSecurity,
+      rugCheckSecurity,
+      validatedData,
+      links,
+      risks,
+      createdAt,
+      developer,
+      tradesData,
+    },
+    rawData: {
+      data1,
+      data2,
+      data3,
+    },
+    isLoading:
+      resp1.isLoading ||
+      resp1.isPending ||
+      resp2.isLoading ||
+      resp2.isPending ||
+      resp3.isLoading,
+  };
+};
