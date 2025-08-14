5d0a3fcbb20d7bda971c619a5c4ab4536ca97392
Majid Pouramini <pouramini.majid@gmail.com>
2025-08-14 17:51:00 +0330

    fix sentry init

--- src/config/sentry.ts
+++ src/config/sentry.ts
@@ -2,7 +2,7 @@ import * as Sentry from '@sentry/react';
 import { isLocal, isProduction } from 'utils/version';
 
 export default function configSentry() {
-  if (isLocal) {
+  if (!isLocal) {
     Sentry.init({
       dsn: 'https://79fe673a0ca923d0ae50321a49d10eec@sentry.wisdomise.com/13',
       integrations: [],
