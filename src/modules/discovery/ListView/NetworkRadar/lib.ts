c837b17d9b41861a5f34f22144c9a958bc533b99
Amir Momenian <nainemom@gmail.com>
2025-08-26 20:51:37 +0330

    add logs

--- src/modules/discovery/ListView/NetworkRadar/lib.ts
+++ src/modules/discovery/ListView/NetworkRadar/lib.ts
@@ -87,6 +87,7 @@ export const useNetworkRadarStream = (
   const migrated = networkRadarGrpc.useTrenchMigratedStreamLastValue(
     filters.migrated,
   );
+  console.log({ newPairs, finalStretch, migrated });
 
   return {
     new_pairs: newPairs,
