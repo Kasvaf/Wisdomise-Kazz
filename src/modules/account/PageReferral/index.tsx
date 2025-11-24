7e7cf1ddbbb34ecc56380c7911bf1cd8547061fd
Majid Pouramini <pouramini.majid@gmail.com>
2025-11-24 14:13:42 +0330

    remove referral onboarding

--- src/modules/account/PageReferral/index.tsx
+++ src/modules/account/PageReferral/index.tsx
@@ -8,12 +8,11 @@ import {
 } from 'api';
 import { bxRightArrowAlt, bxShareAlt } from 'boxicons-quasar';
 import { clsx } from 'clsx';
-import ReferralOnboardingModalContent from 'modules/account/PageReferral/ReferralOnboarding/ReferralOnboardingModalContent';
 import { useReferral } from 'modules/account/PageReferral/useReferral';
 import useRewardModal from 'modules/account/PageRewards/RewardModal/useRewardModal';
 import { SolanaIcon } from 'modules/autoTrader/TokenActivity';
 import PageWrapper from 'modules/base/PageWrapper';
-import { useEffect, useMemo, useRef } from 'react';
+import { useMemo, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import Badge from 'shared/Badge';
@@ -23,11 +22,9 @@ import Icon from 'shared/Icon';
 import { ReadableNumber } from 'shared/ReadableNumber';
 import ReferralQrCode from 'shared/ShareTools/ReferralQrCode';
 import { ReferralShareLinks } from 'shared/ShareTools/ReferralShareLinks';
-import useModal from 'shared/useModal';
 import { Button } from 'shared/v1-components/Button';
 import EditableText from 'shared/v1-components/EditableText';
 import { Table } from 'shared/v1-components/Table';
-import { useLocalStorage } from 'usehooks-ts';
 import { shortenAddress } from 'utils/address';
 import useIsMobile from 'utils/useIsMobile';
 import claimBg from './images/claim-bg.png';
@@ -44,11 +41,11 @@ export default function ReferralPage() {
   const { data: referral, isLoading } = useReferralStatusQuery();
   const { data: referredUsers } = useFriendsQuery();
   const navigate = useNavigate();
-  const [ReferralOnboardingModal, openReferralOnboardingModal] = useModal(
-    ReferralOnboardingModalContent,
-    { fullscreen: true, closable: false },
-  );
-  const [done] = useLocalStorage('referral-onboarding', false);
+  // const [ReferralOnboardingModal, openReferralOnboardingModal] = useModal(
+  //   ReferralOnboardingModalContent,
+  //   { fullscreen: true, closable: false },
+  // );
+  // const [done] = useLocalStorage('referral-onboarding', false);
 
   const {
     mutateAsync: claimTradeReferralAsync,
@@ -96,11 +93,11 @@ export default function ReferralPage() {
     });
   };
 
-  useEffect(() => {
-    if (!done) {
-      void openReferralOnboardingModal({});
-    }
-  }, [done, openReferralOnboardingModal]);
+  // useEffect(() => {
+  //   if (!done) {
+  //     void openReferralOnboardingModal({});
+  //   }
+  // }, [done, openReferralOnboardingModal]);
 
   return (
     <PageWrapper
@@ -112,17 +109,17 @@ export default function ReferralPage() {
         {/* Referral */}
         <div className="rounded-xl bg-v1-surface-l1 p-3">
           <h1 className="mb-3 font-medium">{t('page-referral.title')}</h1>
-          <p className="mb-2 text-v1-content-secondary text-xs">
+          <p className="mb-5 text-v1-content-secondary text-xs">
             {t('page-referral.subtitle')}
           </p>
 
-          <Button
-            className="!text-v1-content-link !p-0 mb-3"
-            onClick={() => openReferralOnboardingModal({})}
-            variant="link"
-          >
-            {t('page-referral.how.button')}
-          </Button>
+          {/* <Button */}
+          {/*   className="!text-v1-content-link !p-0 mb-3" */}
+          {/*   onClick={() => openReferralOnboardingModal({})} */}
+          {/*   variant="link" */}
+          {/* > */}
+          {/*   {t('page-referral.how.button')} */}
+          {/* </Button> */}
 
           {/* Claim Rewards */}
           <div className="relative mb-3 overflow-hidden rounded-xl bg-v1-surface-l2">
@@ -234,21 +231,21 @@ export default function ReferralPage() {
 
         {/* Cashback */}
         <div className="rounded-xl bg-v1-surface-l1 p-3">
-          <h1 className="mb-2 flex items-center gap-2 font-medium">
+          <h1 className="mb-3 flex items-center gap-2 font-medium">
             Cashback
             <Badge color="gradient" label="NEW" />
           </h1>
-          <p className="mb-2 text-v1-content-secondary text-xs">
+          <p className="mb-5 text-v1-content-secondary text-xs">
             Earn SOL from Trading on Solana
           </p>
 
-          <Button
-            className="!text-v1-content-link !p-0 mb-3"
-            onClick={() => openReferralOnboardingModal({})}
-            variant="link"
-          >
-            {t('page-referral.how.button')}
-          </Button>
+          {/* <Button */}
+          {/*   className="!text-v1-content-link !p-0 mb-3" */}
+          {/*   onClick={() => openReferralOnboardingModal({})} */}
+          {/*   variant="link" */}
+          {/* > */}
+          {/*   {t('page-referral.how.button')} */}
+          {/* </Button> */}
 
           {/* Claim Rewards */}
           <div className="relative mb-3 overflow-hidden rounded-xl bg-v1-surface-l2">
@@ -302,7 +299,6 @@ export default function ReferralPage() {
         </div>
       </div>
       {RewardModal}
-      {ReferralOnboardingModal}
     </PageWrapper>
   );
 }
